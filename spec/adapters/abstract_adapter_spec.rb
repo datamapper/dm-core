ce3b04d1b825f8f76e66f3fc329313ab6f4bdead
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-16 20:30:27 -0600

    #first and #all specs in abstractAdapter

--- spec/adapters/abstract_adapter_spec.rb
+++ spec/adapters/abstract_adapter_spec.rb
@@ -10,7 +10,7 @@ describe DataMapper::Adapters::AbstractAdapter do
   it_should_behave_like 'a DataMapper Adapter'
 
   %w{create read update delete}.each do |meth|
-    it "should raise NotImplementedError when #{meth} is called" do
+    it "should raise NotImplementedError when ##{meth} is called" do
       lambda { @adapter.send(meth.intern, nil, nil) }.should raise_error(NotImplementedError)
     end
   end
@@ -29,4 +29,29 @@ describe DataMapper::Adapters::AbstractAdapter do
     @adapter.save(:database_context, instance)
   end
 
+  describe '#first' do
+    it 'should raise an argument error if a limit was set in the query' do
+      lambda { @adapter.first(:repository, Class, :limit => 10) }.should raise_error(ArgumentError)
+    end
+
+    it 'should pass all query options + :limit to #all' do
+      @adapter.should_receive(:all) { |repo, klass, query|
+        repo.should  == :repository
+        klass.should == Class
+        query.should be_kind_of(Hash)
+        query.should have_key(:limit)
+        query[:limit].should == 1
+        query.should have_key(:custom)
+        query[:custom].should == :opts
+
+        [:first_record]
+      }
+
+      @adapter.first(:repository, Class, {:custom => :opts}).should == :first_record
+    end
+  end
+
+  it "should raise NotImplementedError when #all is called" do
+    lambda { @adapter.all(:repository, Class, {}) }.should raise_error(NotImplementedError)
+  end
 end
