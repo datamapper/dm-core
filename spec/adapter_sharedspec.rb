331fab62e6dcce056ffe41cc394b4cd8d2026148
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-16 19:03:19 -0600

    Abstract adapter spec, and a shared spec that all adapters should pass (see AbstractAdapter & DataObjectAdapter specs for examples

---
+++ spec/adapter_sharedspec.rb
@@ -0,0 +1,16 @@
+
+describe "a DataMapper Adapter", :shared => true do
+
+  it "should initialize the connection uri" do
+    new_adapter = @adapter.class.new('some://uri/string')
+    new_adapter.instance_variable_get('@uri').should == 'some://uri/string'
+  end
+
+  %w{create read update delete save} .each do |meth|
+    it "should have a #{meth} method" do
+      @adapter.should respond_to(meth.intern)
+    end
+  end
+
+end
+
--- spec/adapters/data_object_adapter_spec.rb
+++ spec/adapters/data_object_adapter_spec.rb
@@ -1,31 +1,11 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require File.join(File.dirname(__FILE__), '../../lib/data_mapper/adapters/data_object_adapter')
+require File.join(File.dirname(__FILE__), '..', 'adapter_sharedspec')
 
 describe DataMapper::Adapters::DataObjectAdapter do
-  
-  before(:all) do
-    fixtures(:zoos)
-  end
-  
-  if ENV['ADAPTER'] == 'postgresql'
-    it "should be able to be given a port" do
-      options = {
-        :adapter => 'postgresql',
-        :database => 'data_mapper_1',
-        :port => 4001
-      }
-      repo = DataMapper::Repository.setup(:my_postgres, options)
-      repo.port.should == 4001
-    end
-  end
-  
-  it "should use DB defaults when creating an empty record" do
-    comment = Comment.create({})
-    comment.new_record?.should be_false
-  end
-  
-  it "should raise an argument error on create if an attribute value is not a primitive" do
-    lambda { Zoo.create(:name => [nil, 'bob']) }.should raise_error(ArgumentError)
+  before do
+    @adapter = DataMapper::Adapters::DataObjectAdapter.new('mock::/localhost')
   end
 
-  it "should accept a subclass as a valid type if the parent is a valid type"
+  it_should_behave_like 'a DataMapper Adapter'
+
 end
