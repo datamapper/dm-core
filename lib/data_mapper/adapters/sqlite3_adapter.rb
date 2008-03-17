82920e551f5fb670d2df0e73959bc55c4d4e467d
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-16 20:44:24 -0600

    Replaced global constants that were throwing warnings with a #constants method that is easily overridden

--- lib/data_mapper/adapters/sqlite3_adapter.rb
+++ lib/data_mapper/adapters/sqlite3_adapter.rb
@@ -15,130 +15,25 @@ module DataMapper
     
     class Sqlite3Adapter < DataObjectAdapter
       
-      TYPES.merge!({
-        :integer => 'INTEGER'.freeze,
-        :string => 'TEXT'.freeze,
-        :text => 'TEXT'.freeze,
-        :class => 'TEXT'.freeze,
-        :boolean => 'INTEGER'.freeze
-      })
+      def constants
+        super.merge({
+          :column_quoting_character => %{"},
+          :types => {
+            :integer => 'INTEGER'.freeze,
+            :string => 'TEXT'.freeze,
+            :text => 'TEXT'.freeze,
+            :class => 'TEXT'.freeze,
+            :boolean => 'INTEGER'.freeze
+          },
+          :batch_insertable? => false
+        }
+      end
 
-      TABLE_QUOTING_CHARACTER = '"'.freeze
-      COLUMN_QUOTING_CHARACTER = '"'.freeze
-      TRUE_ALIASES << 't'.freeze 
-      FALSE_ALIASES << 'f'.freeze
-      
       def create_connection
         connnection = DataObjects::Sqlite3::Connection.new(@uri)
         # connnection.logger = DataMapper.logger
         return connnection
       end
-      
-      def batch_insertable?
-        false
-      end
-      
-      module Mappings
-                
-        class Table
-          def to_exists_sql
-            @to_exists_sql || @to_exists_sql = <<-EOS.compress_lines
-              SELECT "name"
-              FROM "#{temporary? ? 'sqlite_temp_master' : 'sqlite_master'}"
-              WHERE "type" = "table"
-                AND "name" = ?
-            EOS
-          end
-          
-          def to_column_exists_sql
-            @to_column_exists_sql || @to_column_exists_sql = <<-EOS.compress_lines
-              PRAGMA TABLE_INFO(?)
-            EOS
-          end
-          
-          def to_truncate_sql
-            "DELETE FROM #{to_sql}"
-          end
-          
-          alias_method :to_columns_sql, :to_column_exists_sql
-          
-          def unquote_default(default)
-            default.gsub(/(^'|'$)/, "") rescue default
-          end
-          
-        end # class Table
-        
-        class Column
-          def serial_declaration
-            "AUTOINCREMENT"
-          end
-          
-          def size
-            nil
-          end
-                    
-          def alter!
-            @adapter.connection do |db|
-              flush_sql_caches!
-              backup_table = @adapter.table("#{@table.name}_backup")
-
-              @table.columns.each do |column|
-                backup_table.add_column(column.name, column.type, column.options)
-              end
-
-              backup_table.temporary = true
-
-              backup_table.create!
-              
-              sql = <<-EOS.compress_lines
-                INSERT INTO #{backup_table.to_sql} SELECT #{@table.columns.map { |c| c.to_sql }.join(', ')} FROM #{@table.to_sql};
-                DROP TABLE #{@table.to_sql};
-                #{@table.to_create_sql};
-                INSERT INTO #{@table.to_sql} SELECT #{backup_table.columns.map { |c| c.to_sql }.join(', ')} FROM #{backup_table.to_sql};
-              EOS
-              
-              sql.split(';').each do |part|
-                db.create_command(part).execute_non_query
-              end
-              
-              backup_table.drop!
-              flush_sql_caches!
-            end
-          end
-          
-          def drop!
-            @adapter.connection do |db|
-              @table.columns.delete(self)
-              flush_sql_caches!
-              
-              backup_table = @adapter.table("#{@table.name}_backup")
-              
-              @table.columns.each do |column|
-                backup_table.add_column(column.name, column.type, column.options)
-              end
-
-              backup_table.temporary = true
-
-              backup_table.create!
-              
-              sql = <<-EOS.compress_lines
-                INSERT INTO #{backup_table.to_sql} SELECT #{@table.columns.map { |c| c.to_sql }.join(', ')} FROM #{@table.to_sql};
-                DROP TABLE #{@table.to_sql};
-                #{@table.to_create_sql};
-                INSERT INTO #{@table.to_sql} SELECT #{backup_table.columns.map { |c| c.to_sql }.join(', ')} FROM #{backup_table.to_sql};
-              EOS
-              
-              sql.split(';').each do |part|
-                db.create_command(part).execute_non_query
-              end
-              
-              backup_table.drop!
-              flush_sql_caches!
-            end
-          end          
-          
-        end # class Column
-      end # module Mappings
       
     end # class Sqlite3Adapter
     
