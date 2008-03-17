4e27db10b0ea6ea27935cc2d7815199d4d803af4
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-17 14:41:07 -0600

    Add these perfectly valid options back in

--- rakefile.rb
+++ rakefile.rb
@@ -125,6 +125,8 @@ gem_spec = Gem::Specification.new do |s|
  
   s.require_path = "lib"
   s.requirements << "none"
+  s.executables = ["dm"]
+  s.bindir = "bin"
   s.add_dependency("fastthread")
   s.add_dependency("json")
   s.add_dependency("rspec")
