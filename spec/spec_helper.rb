139f8b6b9ffc9e8dcca96cc33eb471fa9e7a0b5f
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-18 17:05:21 -0600

    Change AbstractAdapter API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'pp'
 
+# for __DIR__
+require File.join(File.dirname(__FILE__), '..', 'lib', 'data_mapper', 'support', 'kernel')
+
 ENV['LOG_NAME'] = 'spec'
 require File.join(File.dirname(__FILE__), '..', 'environment')
