7ce6db23979d6b909f7b4a64c82cca59e9652ef0
Mayo Jordanov <mayo@a226.blocka-154.stargate.ca>
2008-03-18 22:07:29 -0700

    Type support and some specs

--- lib/data_mapper/type.rb
+++ lib/data_mapper/type.rb
@@ -1,17 +1,40 @@
+#require __DIR__ + 'property'
+
 module DataMapper
   class Type
-    
-    class << self
-      attr_accessor :primitive #map to Ruby type
+    #TODO: figure out a way to read this from DataMapper::Property without cyclic require(s)
+    #This should ALWAYS mirror DataMapper::Property::PROPERTY_OPTIONS
+    PROPERTY_OPTIONS = [ :public, :protected, :private, :accessor, :reader, :writer, :lazy,
+      :default, :nullable, :key, :serial, :field, :size, :length,
+      :format, :index, :check, :ordinal, :auto_validation ]
       
+    class << self
+      #attr_accessor :primitive #map to Ruby type
+      
+      def primitive(primitive = nil)
+        return @primitive if primitive.nil?
+        
+        @primitive = primitive
+      end
+
       #load DataMapper::Property options
-      DataMapper::Property::PROPERTY_OPTIONS.each do |property_option|
-        attr_accessor property_option
+      # DataMapper::Property::PROPERTY_OPTIONS.each do |property_option|
+      PROPERTY_OPTIONS.each do |property_option|
+        # attr_accessor property_option
+        
+        self.class_eval <<-EOS
+        def #{property_option}(#{property_option} = nil)
+          return @#{property_option} if #{property_option}.nil?
+          
+          @#{property_option} = #{property_option}
+        end
+        EOS
       end
 
       def options
-        public_methods.inject({}) do |h,m|
-          h[m.to_sym] = send(m); h
+        PROPERTY_OPTIONS.inject({}) do |options, method|
+          value = send(method)
+          options[method.to_sym] = value unless value.nil?; options
         end
       end
     end
