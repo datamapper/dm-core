a3f66dd710fed9a8fc4492a3471db5334e5e6756
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-03-24 01:36:58 -0700

    Speced out DataMapper::Query
    * Added assertion to DataMapper::Query#initialize that options be a Hash
    * Updated assertion for options[:offset] to ensure it is greater than
      or equal to 0
    * Updated assertion for options[:limit] to ensure it is greater than
      or equal to 1
    * Updated DataMapper::Query#update_conditions! to process
      options[:conditions] when it is a one or two element Array
    * Fixed String handling for options[:conditions], was not moving
      the elements at index 1+ to a new Array in the condition tuple

--- lib/data_mapper/query.rb
+++ lib/data_mapper/query.rb
@@ -1,5 +1,3 @@
-# TODO: define exception classes instead of using ArgumentError everywhere
-
 module DataMapper
   class Query
     # XXX: since :fields and :conditions are plural, shouldn't :link and :include
@@ -11,29 +9,35 @@ module DataMapper
     attr_reader :resource, *OPTIONS
 
     def initialize(resource, options = {})
+      # validate the resource and options
       raise ArgumentError, "resource must be a Class, but is #{resource.class}" unless resource.kind_of?(Class)
-      raise ArgumentError, 'resource must include DataMapper::Resource' unless resource.included_modules.include?(DataMapper::Resource)
+      raise ArgumentError, 'resource must include DataMapper::Resource'         unless resource.included_modules.include?(DataMapper::Resource)
+      raise ArgumentError, 'options must be a Hash'                             unless options.kind_of?(Hash)
 
+      # validate the reload option
       if options.has_key?(:reload) && options[:reload] != true && options[:reload] != false
         raise ArgumentError, ":reload must be true or false, but was #{options[:reload].inspect}"
       end
 
+      # validate the offset and limit options
       ([ :offset, :limit ] & options.keys).each do |attribute|
         value = options[attribute]
         raise ArgumentError, ":#{attribute} must be an Integer, but was #{value.class}" unless value.kind_of?(Integer)
-        raise ArgumentError, ":#{attribute} must be greater than or equal to 0" unless value >= 0
       end
+      raise ArgumentError, ':offset must be greater than or equal to 0' if options.has_key?(:offset) && !(options[:offset] >= 0)
+      raise ArgumentError, ':limit must be greater than or equal to 1'  if options.has_key?(:limit)  && !(options[:limit]  >= 1)
 
+      # validate the order, fields, link, include and conditions options
       ([ :order, :fields, :link, :include, :conditions ] & options.keys).each do |attribute|
         value = options[attribute]
         raise ArgumentError, ":#{attribute} must be an Array, but was #{value.class}" unless value.kind_of?(Array)
-        raise ArgumentError, ":#{attribute} cannot be an empty Array" unless value.any?
+        raise ArgumentError, ":#{attribute} cannot be an empty Array"                 unless value.any?
       end
 
       @resource   = resource                       # must be Class that includes DataMapper::Resource
       @reload     = options.fetch :reload,  false  # must be true or false
-      @offset     = options.fetch :offset,  0      # must be an Integer equal to or greater than 0
-      @limit      = options.fetch :limit,   nil    # must be an Integer equal to or greater than 0
+      @offset     = options.fetch :offset,  0      # must be an Integer greater than or equal to 0
+      @limit      = options.fetch :limit,   nil    # must be an Integer greater than or equal to 1
       @order      = options.fetch :order,   []     # TODO: must be an Array of ??
       @fields     = options.fetch :fields,  []     # TODO: must be an Array of ??
       @link       = options.fetch :link,    []     # TODO: must be an Array of ??
@@ -46,11 +50,16 @@ module DataMapper
 
       if conditions_option = options[:conditions]
         clause = conditions_option.shift
-        append_condition!(clause, conditions_option.any? ? conditions_option : nil)
+        append_condition!(clause, conditions_option.any? ? conditions_option.dup : nil)
       end
     end
 
     def update(other)
+      # TODO: assert that other must be a DataMapper::Query object or a Hash
+
+      # TODO: ask ssmoot if the other's resource doesn't make self.resource,
+      # should we overwrite the resource or throw an exception?
+
       other = self.class.new(resource, other) if other.kind_of?(Hash)
 
       @resource, @reload = other.resource, other.reload
@@ -73,6 +82,7 @@ module DataMapper
     end
 
     def ==(other)
+      return true if super
       @resource == other.resource &&
       @reload   == other.reload   &&
       @offset   == other.offset   &&
@@ -98,7 +108,7 @@ module DataMapper
       @conditions << case clause
         when Symbol::Operator : [ clause.type, clause.value, value ]
         when Symbol           : [ :eql,        clause,       value ]
-        when String           : [ clause, value ]  # return a pair when passed in raw SQL
+        when String           : value.nil? ? [ clause ] : [ clause, Array(value) ]  # when passed in a raw Query
         else raise ArgumentError, "Condition type #{clause.inspect} not supported"
       end
     end
@@ -108,6 +118,11 @@ module DataMapper
     # [ :gt, :amount, 5 ] and the other's condition is [ :lt, :amount, 2 ]
     # there is a conflict.  When in conflict the other's conditions
     # overwrites self's conditions.
+
+    # TODO: Another condition is when the other condition operator is
+    # eql, this should over-write all the like,range and list operators
+    # for the same clause, since we are now looking for an exact match.
+    # Vice versa, passing in eql should overwrite all of those operators.
 
     def update_conditions!(other)
 
@@ -123,25 +138,28 @@ module DataMapper
       # loop over each of the other's conditions, and overwrite the
       # conditions when in conflict
       other.conditions.each do |other_condition|
-        next unless other_condition.size == 3  # only process triplets
-        other_operator, other_clause, other_value = *other_condition
+        if other_condition.size == 3 # only process triplets
+          other_operator, other_clause, other_value = *other_condition
 
-        if condition = conditions_index[other_clause][other_operator]
-          # if the other condition matches an existing condition, and
-          # the operators match, then overwrite it
-          operator, clause, value = *condition
+          if condition = conditions_index[other_clause][other_operator]
+            # if the other condition matches an existing condition, and
+            # the operators match, then overwrite it
+            operator, clause, value = *condition
 
-          condition[2] = case operator
-            when :eql, :like : other_value
-            when :gt,  :gte  : [ value, other_value ].min
-            when :lt,  :lte  : [ value, other_value ].max
-            when :not, :in   : Array(value) | Array(other_value)
-            else value
+            condition[2] = case operator
+              when :eql, :like : other_value
+              when :gt,  :gte  : [ value, other_value ].min
+              when :lt,  :lte  : [ value, other_value ].max
+              when :not, :in   : Array(value) | Array(other_value)
+              else value
+            end
+
+            next  # process the next other condition
           end
-        else
-          # otherwise append the other condition
-          @conditions << [ other_operator, other_clause, other_value ]
         end
+
+        # otherwise append the other condition
+        @conditions << [ other_operator, other_clause, other_value ]
       end
     end
   end # class Query
---
+++ spec/query_spec.rb
@@ -0,0 +1,353 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path + 'spec_helper'
+
+describe DataMapper::Query do
+  GOOD_OPTIONS = [
+    [ :reload,  false,     ],
+    [ :reload,  true,      ],
+    [ :offset,  0,         ],
+    [ :offset,  1,         ],
+    [ :limit,   1,         ],
+    [ :limit,   2,         ],
+    [ :order,   [ :stub ], ], # TODO: fill in allowed default value
+    [ :fields,  [ :stub ], ], # TODO: fill in allowed default value
+    [ :link,    [ :stub ], ], # TODO: fill in allowed default value
+    [ :include, [ :stub ], ], # TODO: fill in allowed default value
+  ]
+
+  BAD_OPTIONS = {
+    :reload     => 'true',
+    :offset     => -1,
+    :limit      => 0,
+    :order      => [],
+    :fields     => [],
+    :link       => [],
+    :include    => [],
+    :conditions => [],
+  }
+
+  # flatten Array into a Hash, and remove default values
+  UPDATED_OPTIONS = GOOD_OPTIONS.inject({}) do |options,(attribute,value)|
+    options.update attribute => value
+  end
+
+  before :all do
+    class Article
+      include DataMapper::Resource
+    end
+
+    class Comment
+      include DataMapper::Resource
+    end
+
+    class NormalClass
+      # should not include DataMapper::Resource
+    end
+  end
+
+  describe '.new' do
+    describe 'should set the attribute' do
+      it '#resource with resource' do
+        query = DataMapper::Query.new(Article)
+        query.resource.should == Article
+      end
+
+      GOOD_OPTIONS.each do |(attribute,value)|
+        it "##{attribute} with options[:#{attribute}]" do
+          query = DataMapper::Query.new(Article, attribute => value)
+          query.send(attribute).should == value
+        end
+      end
+
+      describe ' #conditions with options[:conditions]' do
+        it 'when they have a one element Array' do
+          query = DataMapper::Query.new(Article, :conditions => [ 'name = "dkubb"' ])
+          query.conditions.should == [ [ 'name = "dkubb"' ] ]
+        end
+
+        it 'when they have a two or more element Array' do
+          query = DataMapper::Query.new(Article, :conditions => [ 'name = ?', 'dkubb' ])
+          query.conditions.should == [ [ 'name = ?', [ 'dkubb' ] ] ]
+        end
+      end
+
+      describe ' #conditions with unknown options' do
+        it 'when a Symbol object is a key' do
+          query = DataMapper::Query.new(Article, :author => 'dkubb')
+          query.conditions.should == [ [ :eql, :author, 'dkubb' ] ]
+        end
+
+        it 'when a Symbol::Operator object is a key' do
+          query = DataMapper::Query.new(Article, :author.like => /\Ad(?:an\.)kubb\z/)
+          query.conditions.should == [ [ :like, :author, /\Ad(?:an\.)kubb\z/ ] ]
+        end
+      end
+    end
+
+    describe 'should raise an ArgumentError' do
+      it 'when resource is nil' do
+        lambda {
+          DataMapper::Query.new(nil)
+        }.should raise_error(ArgumentError)
+      end
+
+      it 'when resource is a Class that does not include DataMapper::Resource' do
+        lambda {
+          DataMapper::Query.new(NormalClass)
+        }.should raise_error(ArgumentError)
+      end
+
+      it 'when options is not a Hash' do
+        lambda {
+          DataMapper::Query.new(Article, nil)
+        }.should raise_error(ArgumentError)
+      end
+
+      BAD_OPTIONS.each do |attribute,value|
+        it "when options[:#{attribute}] is nil" do
+          lambda {
+            DataMapper::Query.new(Article, attribute => nil)
+          }.should raise_error(ArgumentError)
+        end
+
+        it "when options[:#{attribute}] is #{value.kind_of?(Array) && value.empty? ? 'an empty Array' : value.inspect}" do
+          lambda {
+            DataMapper::Query.new(Article, attribute => value)
+          }.should raise_error(ArgumentError)
+        end
+      end
+
+      it 'when unknown options use something that is not a Symbol::Operator, Symbol or String is a key' do
+        lambda {
+          DataMapper::Query.new(Article, nil => nil)
+        }.should raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#update' do
+    before do
+      @query = DataMapper::Query.new(Article,
+        :offset  => 1,
+        :limit   => 1,
+        :order   => [ :stub ],
+        :fields  => [ :stub ],
+        :link    => [ :stub ],
+        :include => [ :stub ]
+      )
+    end
+
+    it 'should instantiate a DataMapper::Query object from other when it is a Hash' do
+      other = { :reload => :true }
+
+      mock_query_class = mock('DataMapper::Query class')
+      @query.should_receive(:class).with(no_args).ordered.and_return(mock_query_class)
+      mock_query_class.should_receive(:new).with(@query.resource, other).ordered.and_return(@query)
+
+      @query.update(other)
+    end
+
+    it 'should return self' do
+      other = DataMapper::Query.new(Article)
+      @query.update(other).should == @query
+    end
+
+    describe 'should overwrite the attribute' do
+      it '#resource with other resource' do
+        other = DataMapper::Query.new(Comment)
+        @query.update(other).resource.should == Comment
+      end
+
+      it '#reload with other reload' do
+        other = DataMapper::Query.new(Comment, :reload => true)
+        @query.update(other).reload.should == true
+      end
+
+      it '#offset with other offset when it is not equal to 0' do
+        other = DataMapper::Query.new(Comment, :offset => 1)
+        @query.update(other).offset.should == 1
+      end
+
+      it '#limit with other limit when it is not nil' do
+        other = DataMapper::Query.new(Comment, :limit => 1)
+        @query.update(other).limit.should == 1
+      end
+
+      [ :eql, :like ].each do |operator|
+        it "#conditions with other conditions when updating the '#{operator}' clause to a different value than in self" do
+          # set the initial conditions
+          @query.update(:author.send(operator) => 'ssmoot')
+
+          # update the conditions, and overwrite with the new value
+          other = DataMapper::Query.new(Comment, :author.send(operator) => 'dkubb')
+          @query.update(other).conditions.should == [ [ operator, :author, 'dkubb' ] ]
+        end
+      end
+
+      [ :gt, :gte ].each do |operator|
+        it "#conditions with other conditions when updating the '#{operator}' clause to a value less than in self" do
+          # set the initial conditions
+          @query.update(:created_at.send(operator) => Time.at(1))
+
+          # update the conditions, and overwrite with the new value is less
+          other = DataMapper::Query.new(Comment, :created_at.send(operator) => Time.at(0))
+          @query.update(other).conditions.should == [ [ operator, :created_at, Time.at(0) ] ]
+        end
+      end
+
+      [ :lt, :lte ].each do |operator|
+        it "#conditions with other conditions when updating the '#{operator}' clause to a value greater than in self" do
+          # set the initial conditions
+          @query.update(:created_at.send(operator) => Time.at(0))
+
+          # update the conditions, and overwrite with the new value is more
+          other = DataMapper::Query.new(Comment, :created_at.send(operator) => Time.at(1))
+          @query.update(other).conditions.should == [ [ operator, :created_at, Time.at(1) ] ]
+        end
+      end
+    end
+
+    describe 'should append the attribute' do
+      [ :order, :fields, :link, :include ].each do |attribute|
+        it "##{attribute} with other #{attribute} unique values" do
+          other = DataMapper::Query.new(Comment, attribute => [ :other, :stub ])
+          @query.update(other).send(attribute).should == [ :stub, :other ]
+        end
+      end
+
+      it '#conditions with other conditions when they are unique' do
+        # set the initial conditions
+        @query.update(:title => 'On DataMapper')
+
+        # update the conditions, but merge the conditions together
+        other = DataMapper::Query.new(Comment, :author => 'dkubb')
+        @query.update(other).conditions == [ [ :eql, :title, 'On DataMapper' ], [ :eql, :author, 'dkubb' ] ]
+      end
+
+      [ :not, :in ].each do |operator|
+        it "#conditions with other conditions when updating the '#{operator}' clause" do
+          # set the initial conditions
+          @query.update(:created_at.send(operator) => [ Time.at(0) ])
+
+          # update the conditions, and overwrite with the new value is more
+          other = DataMapper::Query.new(Comment, :created_at.send(operator) => [ Time.at(1) ])
+          @query.update(other).conditions.should == [ [ operator, :created_at, [ Time.at(0), Time.at(1) ] ] ]
+        end
+      end
+
+      it '#conditions with other conditions when they have a one element condition' do
+        # set the initial conditions
+        @query.update(:title => 'On DataMapper')
+
+        # update the conditions, but merge the conditions together
+        other = DataMapper::Query.new(Comment, :conditions => [ 'author = "dkubb"' ] )
+        @query.update(other).conditions == [ [ :eql, :title, 'On DataMapper' ], [ 'author = "dkubb"' ] ]
+      end
+
+      it '#conditions with other conditions when they have a two or more element condition' do
+        # set the initial conditions
+        @query.update(:title => 'On DataMapper')
+
+        # update the conditions, but merge the conditions together
+        other = DataMapper::Query.new(Comment, :conditions => [ 'author = ?', 'dkubb' ] )
+        @query.update(other).conditions == [ [ :eql, :title, 'On DataMapper' ], [ 'author = ?', [ 'dkubb' ] ] ]
+      end
+    end
+
+    describe 'should not update the attribute' do
+      it '#offset when other offset is equal to 0' do
+        other = DataMapper::Query.new(Comment, :offset => 0)
+        other.offset.should == 0
+        @query.update(other).offset.should == 1
+      end
+
+      it '#limit when other limit is nil' do
+        other = DataMapper::Query.new(Comment)
+        other.limit.should be_nil
+        @query.update(other).offset.should == 1
+      end
+
+      [ :gt, :gte ].each do |operator|
+        it "#conditions with other conditions when they have a '#{operator}' clause with a value greater than in self" do
+          # set the initial conditions
+          @query.update(:created_at.send(operator) => Time.at(0))
+
+          # do not overwrite with the new value if it is more
+          other = DataMapper::Query.new(Comment, :created_at.send(operator) => Time.at(1))
+          @query.update(other).conditions.should == [ [ operator, :created_at, Time.at(0) ] ]
+        end
+      end
+
+      [ :lt, :lte ].each do |operator|
+        it "#conditions with other conditions when they have a '#{operator}' clause with a value less than in self" do
+          # set the initial conditions
+          @query.update(:created_at.send(operator) => Time.at(1))
+
+          # do not overwrite with the new value if it is less
+          other = DataMapper::Query.new(Comment, :created_at.send(operator) => Time.at(0))
+          @query.update(other).conditions.should == [ [ operator, :created_at, Time.at(1) ] ]
+        end
+      end
+    end
+  end
+
+  describe '#merge' do
+    it 'should pass arguments as-is to duplicate object\'s #update method' do
+      query      = DataMapper::Query.new(Article)
+      dupe_query = query.dup
+
+      query.should_receive(:dup).with(no_args).ordered.and_return(dupe_query)
+      dupe_query.should_receive(:update).with(:author => 'dkubb').ordered
+
+      query.merge(:author => 'dkubb')
+    end
+  end
+
+  describe '#==' do
+    before do
+      @query = DataMapper::Query.new(Article)
+    end
+
+    describe 'should be equal' do
+      it 'when other is same object' do
+        @query.update(:author => 'dkubb').should == @query
+      end
+
+      it 'when other has the same attributes' do
+        other = DataMapper::Query.new(Article)
+        @query.object_id.should_not == other.object_id
+        @query.should == other
+      end
+
+      it 'when other has the same conditions sorted differently' do
+        @query.update(:author => 'dkubb')
+        @query.update(:title  => 'On DataMapper')
+
+        other = DataMapper::Query.new(Article, :title => 'On DataMapper')
+        other.update(:author => 'dkubb')
+
+        # query conditions are in different order
+        @query.conditions.should == [ [ :eql, :author, 'dkubb' ],        [ :eql, :title, 'On DataMapper' ] ]
+        other.conditions.should  == [ [ :eql, :title, 'On DataMapper' ], [ :eql, :author, 'dkubb' ]        ]
+
+        @query.should == other
+      end
+    end
+
+    describe 'should be different' do
+      it 'when other resource is different than self.resource' do
+        @query.should_not == DataMapper::Query.new(Comment)
+      end
+
+      UPDATED_OPTIONS.each do |attribute,value|
+        it "when other #{attribute} is different than self.#{attribute}" do
+          @query.should_not == DataMapper::Query.new(Article, attribute => value)
+        end
+      end
+
+      it 'when other conditions are different than self.conditions' do
+        @query.should_not == DataMapper::Query.new(Article, :author => 'dkubb')
+      end
+    end
+  end
+end
