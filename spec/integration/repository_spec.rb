96fe8f50be7a4ea5d58b7f49b3ea3e6c59cdf0c0
Guy van den Berg <vandenberg.guy@gmail.com>
2008-04-03 09:17:16 +0200

    Cleaned up sqlite3_adapter_spec. Moved many things to their own specs in integration.   * associations   * query   * repository   * property   * type

---
+++ spec/integration/repository_spec.rb
@@ -0,0 +1,76 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
+
+require __DIR__.parent.parent + 'lib/data_mapper'
+
+
+begin
+  require 'do_sqlite3'
+
+  DataMapper.setup(:sqlite3, "sqlite3://#{__DIR__}/integration_test.db")
+
+  describe DataMapper::Repository do
+    describe "finders" do
+      before do
+        class SerialFinderSpec
+          include DataMapper::Resource
+
+          property :id, Fixnum, :serial => true
+          property :sample, String
+        end
+
+        @adapter = repository(:sqlite3).adapter
+
+        @adapter.execute(<<-EOS.compress_lines)
+          CREATE TABLE "serial_finder_specs" (
+            "id" INTEGER PRIMARY KEY,
+            "sample" VARCHAR(50)
+          )
+        EOS
+
+        # Why do we keep testing with Repository instead of the models directly?
+        # Just because we're trying to target the code we're actualling testing
+        # as much as possible.
+        setup_repository = repository(:sqlite3)
+        100.times do
+          setup_repository.save(SerialFinderSpec.new(:sample => rand.to_s))
+        end
+      end
+
+      it "should return all available rows" do
+        repository(:sqlite3).all(SerialFinderSpec, {}).should have(100).entries
+      end
+
+      it "should allow limit and offset" do
+        repository(:sqlite3).all(SerialFinderSpec, { :limit => 50 }).should have(50).entries
+
+        repository(:sqlite3).all(SerialFinderSpec, { :limit => 20, :offset => 40 }).map(&:id).should ==
+          repository(:sqlite3).all(SerialFinderSpec, {})[40...60].map(&:id)
+      end
+
+      it "should lazy-load missing attributes" do
+        sfs = repository(:sqlite3).all(SerialFinderSpec, { :fields => [:id], :limit => 1 }).first
+        sfs.should be_a_kind_of(SerialFinderSpec)
+        sfs.should_not be_a_new_record
+
+        sfs.instance_variables.should_not include('@sample')
+        sfs.sample.should_not be_nil
+      end
+
+      it "should translate an Array to an IN clause" do
+        ids = repository(:sqlite3).all(SerialFinderSpec, { :limit => 10 }).map(&:id)
+        results = repository(:sqlite3).all(SerialFinderSpec, { :id => ids })
+
+        results.size.should == 10
+        results.map(&:id).should == ids
+      end
+
+      after do
+        @adapter.execute('DROP TABLE "serial_finder_specs"')
+      end
+
+    end
+  end
+rescue LoadError
+  warn "integration/repository_spec not run! Could not load do_sqlite3."
+end  
--- spec/integration/type_spec.rb
+++ spec/integration/type_spec.rb
@@ -2,100 +2,107 @@ require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 require 'faster_csv'
 
-DataMapper.setup(:sqlite3, "sqlite3://#{__DIR__}/integration_test.db") unless DataMapper::Repository.adapters[:sqlite3]
 
-describe DataMapper::Type do
+begin
+  require 'do_sqlite3'
+    
+  DataMapper.setup(:sqlite3, "sqlite3://#{__DIR__}/integration_test.db") unless DataMapper::Repository.adapters[:sqlite3]
 
-  before do
-    
-    @adapter = repository(:sqlite3).adapter
-    @adapter.execute("CREATE TABLE coconuts (id INTEGER PRIMARY KEY, faked TEXT, document TEXT)")
-    
-    module TypeTests
-      class Csv < DataMapper::Type
-        primitive String
-        size 65535
-        lazy true
+  describe DataMapper::Type do
+
+    before do
       
-        def self.load(value)
-          case value
-          when String then FasterCSV.parse(value)
-          when Array then value
-          else nil
+      @adapter = repository(:sqlite3).adapter
+      @adapter.execute("CREATE TABLE coconuts (id INTEGER PRIMARY KEY, faked TEXT, document TEXT)")
+      
+      module TypeTests
+        class Csv < DataMapper::Type
+          primitive String
+          size 65535
+          lazy true
+        
+          def self.load(value)
+            case value
+            when String then FasterCSV.parse(value)
+            when Array then value
+            else nil
+            end
+          end
+          
+          def self.dump(value)
+            case value
+            when Array then
+              FasterCSV.generate do |csv|
+                value.each { |row| csv << row }
+              end
+            when String then value
+            else nil
+            end  
           end
         end
         
-        def self.dump(value)
-          case value
-          when Array then
-            FasterCSV.generate do |csv|
-              value.each { |row| csv << row }
-            end
-          when String then value
-          else nil
-          end  
+        class Impostor < DataMapper::Type
+          primitive String
+        end
+        
+        class Coconut
+          include DataMapper::Resource
+          
+          resource_names[:sqlite3] = 'coconuts'
+          
+          property :id, Fixnum, :serial => true
+          property :faked, Impostor
+          property :document, Csv
         end
       end
       
-      class Impostor < DataMapper::Type
-        primitive String
-      end
-      
-      class Coconut
-        include DataMapper::Resource
-        
-        resource_names[:sqlite3] = 'coconuts'
-        
-        property :id, Fixnum, :serial => true
-        property :faked, Impostor
-        property :document, Csv
-      end
+      @document = <<-EOS.margin
+        NAME, RATING, CONVENIENCE
+        Freebird's, 3, 3
+        Whataburger, 1, 5
+        Jimmy John's, 3, 4
+        Mignon, 5, 2
+        Fuzi Yao's, 5, 1
+        Blue Goose, 5, 1 
+      EOS
     end
     
-    @document = <<-EOS.margin
-      NAME, RATING, CONVENIENCE
-      Freebird's, 3, 3
-      Whataburger, 1, 5
-      Jimmy John's, 3, 4
-      Mignon, 5, 2
-      Fuzi Yao's, 5, 1
-      Blue Goose, 5, 1 
-    EOS
-  end
-  
-  it "should instantiate an object with custom types" do
-    coconut = TypeTests::Coconut.new(:faked => 'bob', :document => @document)
-    coconut.faked.should == 'bob'
-    coconut.document.should be_a_kind_of(Array)
-  end
-  
-  it "should CRUD an object with custom types" do
-    repository(:sqlite3) do
+    it "should instantiate an object with custom types" do
       coconut = TypeTests::Coconut.new(:faked => 'bob', :document => @document)
-      coconut.save.should be_true
-      coconut.id.should_not be_nil
+      coconut.faked.should == 'bob'
+      coconut.document.should be_a_kind_of(Array)
+    end
     
-      fred = TypeTests::Coconut[coconut.id]
-      fred.faked.should == 'bob'
-      fred.document.should be_a_kind_of(Array)
+    it "should CRUD an object with custom types" do
+      repository(:sqlite3) do
+        coconut = TypeTests::Coconut.new(:faked => 'bob', :document => @document)
+        coconut.save.should be_true
+        coconut.id.should_not be_nil
+      
+        fred = TypeTests::Coconut[coconut.id]
+        fred.faked.should == 'bob'
+        fred.document.should be_a_kind_of(Array)
 
-      texadelphia = ["Texadelphia", "5", "3"]
-      
-      # Figure out how to track these... possibly proxies? :-p
-      document = fred.document.dup
-      document << texadelphia
-      fred.document = document
-      
-      fred.save.should be_true
-      
-      # Can't call coconut.reload! since coconut.loaded_set isn't setup.
-      mac = TypeTests::Coconut[fred.id]
-      mac.document.last.should == texadelphia
+        texadelphia = ["Texadelphia", "5", "3"]
+        
+        # Figure out how to track these... possibly proxies? :-p
+        document = fred.document.dup
+        document << texadelphia
+        fred.document = document
+        
+        fred.save.should be_true
+        
+        # Can't call coconut.reload! since coconut.loaded_set isn't setup.
+        mac = TypeTests::Coconut[fred.id]
+        mac.document.last.should == texadelphia
+      end
+    end
+    
+    after do
+      @adapter = repository(:sqlite3).adapter
+      @adapter.execute("DROP TABLE coconuts")
     end
   end
-  
-  after do
-    @adapter = repository(:sqlite3).adapter
-    @adapter.execute("DROP TABLE coconuts")
-  end
-end
+rescue LoadError
+  warn "integration/type_spec not run! Could not load do_sqlite3."
+end    
