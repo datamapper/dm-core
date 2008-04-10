328fc5536b4d1e1724ed8375b4b450810e1a2f81
david <dgleal@gmail.com>
2008-04-11 00:35:17 +0100

    Make the path absolute when the file is not found (sqlite3).

--- lib/data_mapper/adapters/sqlite3_adapter.rb
+++ lib/data_mapper/adapters/sqlite3_adapter.rb
@@ -7,12 +7,12 @@ module DataMapper
     class Sqlite3Adapter < DataObjectsAdapter
       
       TYPES.merge!({
-        :integer => 'INTEGER'.freeze,
-        :string  => 'TEXT'.freeze,
-        :text    => 'TEXT'.freeze,
-        :class   => 'TEXT'.freeze,
-        :boolean => 'INTEGER'.freeze
-      })
+	  :integer => 'INTEGER'.freeze,
+	  :string  => 'TEXT'.freeze,
+	  :text    => 'TEXT'.freeze,
+	  :class   => 'TEXT'.freeze,
+	  :boolean => 'INTEGER'.freeze
+	})
 
       def create_connection
         connnection = DataObjects::Sqlite3::Connection.new(@uri)
@@ -20,6 +20,11 @@ module DataMapper
         return connnection
       end
 
+      def uri(uri_or_options)
+	uri = super(uri_or_options)
+	uri.path = File.join(Dir.pwd, File.dirname(uri.path), File.basename(uri.path)) unless File.exists?(uri.path)
+	uri
+      end
     end # class Sqlite3Adapter
     
   end # module Adapters
