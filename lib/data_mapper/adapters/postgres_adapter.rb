faf31131ab16a287d11be259ed652231cda81b45
Ben Burkert <ben@benburkert.com>
2008-04-16 01:27:02 -0500

    tweaked the postgres adapter to work with 8.3

--- lib/data_mapper/adapters/postgres_adapter.rb
+++ lib/data_mapper/adapters/postgres_adapter.rb
@@ -5,8 +5,18 @@ module DataMapper
   module Adapters
 
     class PostgresAdapter < DataObjectsAdapter
+      
+      TYPES = DataObjectsAdapter::TYPES.merge!({
+        DateTime => 'timestamp'.freeze
+      })
 
       def create_with_returning?; true; end
+      
+      def drop_table_statement(model)
+        <<-EOS.compress_lines
+          DROP TABLE IF EXISTS #{quote_table_name(model.storage_name(name))}
+        EOS
+      end
 
     end # class PostgresAdapter
 
