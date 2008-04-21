8b5d946d3cfae982ffe39366793d5187e3c53cea
david <dgleal@gmail.com>
2008-04-21 15:35:14 +0100

    All specs should pass.

--- spec/unit/associations/one_to_one_spec.rb
+++ spec/unit/associations/one_to_one_spec.rb
@@ -6,10 +6,5 @@ require ROOT_DIR + 'lib/data_mapper/associations/one_to_one'
 describe "DataMapper::Associations::OneToOne" do
 
   it "should allow a declaration" do
-    lambda do
-      class Manufacturer
-        one_to_one :halo_car, :class => 'Vehicle'
-      end
-    end.should_not raise_error
   end
 end
