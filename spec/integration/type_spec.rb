7a2138e803da973d87f4eafaeb0f9e5ea21ed9a2
Sindre Aarsaether <sindre@identu.no>
2008-04-28 21:55:05 +0200

    removed extra datatypes from core (moving them to dm-more)

--- spec/integration/type_spec.rb
+++ spec/integration/type_spec.rb
@@ -14,7 +14,7 @@ begin
     before do
 
       @adapter = repository(:sqlite3).adapter
-      @adapter.execute("CREATE TABLE coconuts (id INTEGER PRIMARY KEY, faked TEXT, document TEXT, stuff TEXT)")
+      @adapter.execute("CREATE TABLE coconuts (id INTEGER PRIMARY KEY, faked TEXT, active INTEGER, note TEXT)")
 
       module TypeTests
         class Impostor < DataMapper::Type
@@ -28,59 +28,45 @@ begin
 
           property :id, Fixnum, :serial => true
           property :faked, Impostor
-          property :document, DM::Csv
-          property :stuff, DM::Yaml
+          property :active, Boolean
+          property :note, Text
         end
       end
-
-      @document = <<-EOS.margin
-        NAME, RATING, CONVENIENCE
-        Freebird's, 3, 3
-        Whataburger, 1, 5
-        Jimmy John's, 3, 4
-        Mignon, 5, 2
-        Fuzi Yao's, 5, 1
-        Blue Goose, 5, 1
-      EOS
-
-      @stuff = YAML::dump({ 'Happy Cow!' => true, 'Sad Cow!' => false })
+      
+      @active = true
+      @note = "This is a note on our ol' guy bob"
     end
 
     it "should instantiate an object with custom types" do
-      coconut = TypeTests::Coconut.new(:faked => 'bob', :document => @document, :stuff => @stuff)
+      coconut = TypeTests::Coconut.new(:faked => 'bob', :active => @active, :note => @note)
       coconut.faked.should == 'bob'
-      coconut.document.should be_a_kind_of(Array)
-      coconut.stuff.should be_a_kind_of(Hash)
+      coconut.active.should be_a_kind_of(TrueClass)
+      coconut.note.should be_a_kind_of(String)
     end
 
     it "should CRUD an object with custom types" do
       repository(:sqlite3) do
-        coconut = TypeTests::Coconut.new(:faked => 'bob', :document => @document, :stuff => @stuff)
+        coconut = TypeTests::Coconut.new(:faked => 'bob', :active => @active, :note => @note)
         coconut.save.should be_true
         coconut.id.should_not be_nil
 
         fred = TypeTests::Coconut[coconut.id]
         fred.faked.should == 'bob'
-        fred.document.should be_a_kind_of(Array)
-        fred.stuff.should be_a_kind_of(Hash)
+        fred.active.should be_a_kind_of(TrueClass)
+        fred.note.should be_a_kind_of(String)
 
-        texadelphia = ["Texadelphia", "5", "3"]
-
-        # Figure out how to track these... possibly proxies? :-p
-        document = fred.document.dup
-        document << texadelphia
-        fred.document = document
-
-        stuff = fred.stuff.dup
-        stuff['Manic Cow!'] = :maybe
-        fred.stuff = stuff
+        fred.note = "Seems like bob is just mockin' around"
 
         fred.save.should be_true
+        
+        fred.active = false
 
+        fred.save.should be_true
+        
         # Can't call coconut.reload! since coconut.loaded_set isn't setup.
         mac = TypeTests::Coconut[fred.id]
-        mac.document.last.should == texadelphia
-        mac.stuff['Manic Cow!'].should == :maybe
+        mac.active.should == false
+        mac.note.should == "Seems like bob is just mockin' around"
       end
     end
 
