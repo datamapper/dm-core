20b34c73051fed236e6417b48183cacf3c85508a
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-05-06 21:49:07 -0700

    Ensure auto_migrate occurs in the context of the supplied repository

--- lib/data_mapper/auto_migrations.rb
+++ lib/data_mapper/auto_migrations.rb
@@ -7,30 +7,32 @@ module DataMapper
       model.extend ClassMethods
       DataMapper::AutoMigrator.models << model
     end
-    
+
     module ClassMethods
       def auto_migrate!(repository_name = default_repository_name)
-        self.relationships(repository_name).each_pair { |name, relationship| relationship.child_key }
-        repository(repository_name).adapter.destroy_model_storage(repository(repository_name), self)
-        repository(repository_name).adapter.create_model_storage(repository(repository_name), self)
+        repository(repository_name) do |r|
+          relationships(r.name).each_value { |relationship| relationship.child_key }
+          r.adapter.destroy_model_storage(r, self)
+          r.adapter.create_model_storage(r, self)
+        end
       end
     end
   end
-  
+
   class AutoMigrator
-    
+
     def self.models
       @@models ||= []
     end
-    
+
     def self.auto_migrate(repository_name)
       # First ensure that association keys are forced to load.
       models.each do |model|
-        model.relationships(repository_name).each_pair do |name, relationship|
+        model.relationships(repository_name).each_value do |relationship|
           relationship.child_key
         end
       end
-      
+
       models.each do |model|
         model.auto_migrate!(repository_name)
       end
