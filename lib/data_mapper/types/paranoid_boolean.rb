e391f7fac6ce1e90c3447f87633a08cbb0cf41f9
Adam French <adam@adam.wieck.com>
2008-05-07 17:38:48 -0500

    paranoid booleans

---
+++ lib/data_mapper/types/paranoid_boolean.rb
@@ -0,0 +1,23 @@
+module DataMapper
+  module Types
+    class ParanoidBoolean < DataMapper::Type(Boolean)
+      primitive TrueClass
+      default   false
+
+      def self.bind(property)
+        model = property.model
+        repository = property.repository
+
+        model.class_eval <<-EOS
+          def destroy
+            attribute_set(#{property.name.inspect}, true)
+            save
+          end
+        EOS
+      
+        model.send(:scope_stack) << DataMapper::Query.new(repository, model, property.name => nil)
+        
+      end      
+    end
+  end
+end
--- spec/integration/type_spec.rb
+++ spec/integration/type_spec.rb
@@ -85,7 +85,7 @@ begin
       end
     end
 
-    it "should respect paranoia" do
+    it "should respect paranoia with a datetime" do
 
       class Lime
         include DataMapper::Resource
@@ -109,6 +109,32 @@ begin
         repository(:sqlite3).adapter.execute("DROP TABLE limes")
       end
     end
+
+    it "should respect paranoia with a datetime" do
+
+      class Lime
+        include DataMapper::Resource
+        property :id, Fixnum, :serial => true
+        property :color, String
+        property :deleted_at, DataMapper::Types::ParanoidBoolean
+      end
+
+      Lime.auto_migrate!(:sqlite3)
+      
+      repository(:sqlite3) do
+        lime = Lime.new
+        lime.color = 'green'
+        
+        lime.save
+        lime.destroy
+        # lime.deleted_at.should_not be_nil
+        repository(:sqlite3).adapter.query("SELECT count(*) from limes").first.should_not == 0
+        repository(:sqlite3).adapter.query("SELECT * from limes").should_not be_empty
+        
+        repository(:sqlite3).adapter.execute("DROP TABLE limes")
+      end
+    end
+
 
     after do
       @adapter = repository(:sqlite3).adapter
