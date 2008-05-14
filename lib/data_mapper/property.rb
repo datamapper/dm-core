c40a8c4791060f22d32a42a14f25ee147e7ed68e
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-05-14 12:12:38 -0700

    Fixed bug when typecasting "0.0" for Integer property types
    * Added specs for typecasting of all numeric property types
    * Updated typecasting for TrueClass so it typecasts more variants
      of 'true' to TrueClass
    * Fixed bug where if a nil value was typecasted for a TrueClass it
      would be returned as nil, which is wrong. nil now is typecasted to
      false.

--- lib/data_mapper/property.rb
+++ lib/data_mapper/property.rb
@@ -384,12 +384,12 @@ module DataMapper
     #-
     # @private
     def typecast(value)
-      return value if type === value || value.nil?
+      return value if type === value || (value.nil? && type != TrueClass)
 
-      if    type == TrueClass  then true == value || 'true' == value || 1 == value || '1' == value
-      elsif type == String     then String(value)
-      elsif type == Float      then Float(value)
-      elsif type == Fixnum     then Integer(value)
+      if    type == TrueClass  then %w[ true 1 t ].include?(value.to_s.downcase)
+      elsif type == String     then value.to_s
+      elsif type == Float      then value.to_f
+      elsif type == Fixnum     then value.to_i
       elsif type == BigDecimal then BigDecimal(value.to_s)
       elsif type == DateTime   then DateTime.parse(value.to_s)
       elsif type == Date       then Date.parse(value.to_s)
@@ -442,7 +442,7 @@ module DataMapper
       # TODO: This default should move to a DataMapper::Types::Text
       # Custom-Type and out of Property.
       @primitive = @options.fetch(:primitive, @type.respond_to?(:primitive) ? @type.primitive : @type)
-      
+
       @getter   = TrueClass == @primitive ? "#{@name}?".to_sym : @name
       @lock     = @options.fetch(:lock,     false)
       @serial   = @options.fetch(:serial,   false)
