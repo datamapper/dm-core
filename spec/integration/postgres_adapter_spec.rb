3c01f8e8e60ca7c74a146bbef511c24063c49537
Martin Kihlgren <zond@sharkattack.troja.ath.cx>
2008-05-14 15:30:46 +0200

    Separated the mocked PostgresAdapter specs from the non-mocked by creating a unit test for PostgresAdapter.

--- spec/integration/postgres_adapter_spec.rb
+++ spec/integration/postgres_adapter_spec.rb
@@ -30,37 +30,6 @@ if HAS_POSTGRES
         Sputnik.auto_upgrade!(:postgres)
         @adapter.column_exists?("sputniks", "new_prop").should == true
       end
-      it "#upgrade_model_storage should create sequences and then call super" do
-        @adapter.should_receive(:create_connection).at_least(1).times.and_return(@connection)
-        @connection.should_receive(:close).at_least(1).times
-        @adapter.should_receive(:exists?).at_least(1).times.with("sputniks").and_return(true)
-        @adapter.should_receive(:column_exists?).at_least(1).times.with("sputniks", "id").and_return(false)
-        @adapter.should_receive(:column_exists?).at_least(1).times.with("sputniks", "name").and_return(false)
-        @adapter.should_receive(:create_sequence_column).at_least(1).times.with(@connection, Sputnik, Sputnik.properties(:default)[:id])
-        @command.should_receive(:execute_non_query).any_number_of_times.and_return(@result)
-        @result.should_receive(:to_i).any_number_of_times.and_return(1)
-        @connection.should_receive(:create_command).once.with("ALTER TABLE \"sputniks\" ADD COLUMN \"id\" INT4 NOT NULL DEFAULT nextval('sputniks_id_seq') NOT NULL").and_return(@command)
-        @connection.should_receive(:create_command).once.with("ALTER TABLE \"sputniks\" ADD COLUMN \"name\" TEXT").and_return(@command)
-        @adapter.upgrade_model_storage(nil, Sputnik).should == [Sputnik.properties(:default)[:id], Sputnik.properties(:default)[:name]]
-      end
-      it "#create_model_storage should create sequences and then call super" do
-        @adapter.should_receive(:create_connection).at_least(1).times.and_return(@connection)
-        @connection.should_receive(:close).at_least(1).times
-        @adapter.should_receive(:create_sequence_column).at_least(1).times.with(@connection, Sputnik, Sputnik.properties(:default)[:id])
-        @command.should_receive(:execute_non_query).any_number_of_times.with(any_args()).and_return(@result)
-        @result.should_receive(:to_i).any_number_of_times.and_return(1)
-        @connection.should_receive(:create_command).once.with("CREATE TABLE \"sputniks\" (\"id\" INT4 NOT NULL DEFAULT nextval('sputniks_id_seq') NOT NULL, \"name\" TEXT, PRIMARY KEY(\"id\"))").and_return(@command)
-        @adapter.create_model_storage(nil, Sputnik)
-      end
-      it "#destroy_model_storage should drop sequences and then call super" do
-        @adapter.should_receive(:create_connection).at_least(1).times.and_return(@connection)
-        @connection.should_receive(:close).at_least(1).times
-        @adapter.should_receive(:drop_sequence_column).at_least(1).times.with(@connection, Sputnik, Sputnik.properties(:default)[:id])
-        @command.should_receive(:execute_non_query).any_number_of_times.with(any_args()).and_return(@result)
-        @result.should_receive(:to_i).any_number_of_times.and_return(1)
-        @connection.should_receive(:create_command).once.with("DROP TABLE IF EXISTS \"sputniks\"").and_return(@command)
-        @adapter.destroy_model_storage(nil, Sputnik)
-      end
     end
 
     describe "querying metadata" do
