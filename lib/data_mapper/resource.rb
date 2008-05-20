fbfd60087e05666c54b8ef0f793a326cbbeb93f6
Abdul-Rahman Advany <abdulrahman@advany.com>
2008-05-20 09:27:50 +0200

    improve dirty check code (thnx to dkubb)

--- lib/data_mapper/resource.rb
+++ lib/data_mapper/resource.rb
@@ -68,11 +68,13 @@ module DataMapper
       property  = self.class.properties(repository.name)[name]
       ivar_name = property.instance_variable_name
 
+      return if instance_variable_get(ivar_name) == value
+
       if property.lock?
         instance_variable_set("@shadow_#{name}", instance_variable_get(ivar_name))
       end
 
-      dirty_attributes << property if attribute_get(name) != value || new_record?
+      dirty_attributes << property
 
       instance_variable_set(ivar_name, property.custom? ? property.type.dump(value, property) : property.typecast(value))
     end
