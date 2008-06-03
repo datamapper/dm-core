01533e0131907ff1300f7cd65219c32337d44e92
Jarkko Laine <jarkko@jlaine.net>
2008-06-03 21:17:32 +0300

    Cleaning up the logger stuff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,9 +31,7 @@ HAS_SQLITE3  = setup_adapter(:sqlite3,  'sqlite3::memory:')
 HAS_MYSQL    = setup_adapter(:mysql,    'mysql://localhost/dm_core_test')
 HAS_POSTGRES = setup_adapter(:postgres, 'postgres://postgres@localhost/dm_core_test')
 
-DataObjects::Logger.new(DataMapper.root / "spec" / "spec.log", :debug)
-DataObjects::Sqlite3.logger = DataObjects.logger
-DataMapper.logger = DataObjects.logger
+DataMapper::Logger.new(nil, :debug)
 
 class Article
   include DataMapper::Resource
