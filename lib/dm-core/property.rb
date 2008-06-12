258ac48711fca26c69053a203b926da29e126932
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-06-11 22:07:38 -0500

    Undoing a few things which the last commit broke for property tracking.

--- lib/dm-core/property.rb
+++ lib/dm-core/property.rb
@@ -467,7 +467,7 @@ module DataMapper
       @unique_index = @options.fetch(:unique_index, false)
 
       @lazy     = @options.fetch(:lazy,     @type.respond_to?(:lazy) ? @type.lazy : false) && !@key
-      @track    = @options.fetch(:track,    @type.respond_to?(:track) ? @type.track : false) || (IMMUTABLE_TYPES.include?(@primitive) ? :set : :get)
+      @track    = @options.fetch(:track,    @type.respond_to?(:track) ? @type.track : :set) || :set
 
       # assign attributes per-type
       if String == @primitive || Class == @primitive
