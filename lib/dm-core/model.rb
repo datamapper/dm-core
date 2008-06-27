7370793f7a6ecf7793819c978fb1bf1e2c1a9464
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-06-27 16:45:17 -0700

    Refactored Model#properties_with_subclasses to initialize the child_key

--- lib/dm-core/model.rb
+++ lib/dm-core/model.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module DataMapper
   module Model
     ##
@@ -130,28 +132,24 @@ module DataMapper
       property
     end
 
-    # TODO: make this a Set?
     def repositories
-      [ repository ] + @properties.keys.collect { |repository_name| DataMapper.repository(repository_name) }
+      [ repository ].to_set + @properties.keys.collect { |repository_name| DataMapper.repository(repository_name) }
     end
 
     def properties(repository_name = default_repository_name)
       @properties[repository_name]
     end
 
+    # @api private
     def properties_with_subclasses(repository_name = default_repository_name)
-      #return properties if we're not interested in sti
-     if @properties[repository_name].inheritance_property.nil?
-       @properties[repository_name]
-     else
-        props = @properties[repository_name].dup
-        self.child_classes.each do |subclass|
-          subclass.properties(repository_name).each do |subprop|
-            props << subprop if not props.any? { |prop| prop.name == subprop.name }
-          end
+      properties = PropertySet.new
+      ([ self ] + (respond_to?(:child_classes) ? child_classes : [])).each do |model|
+        model.relationships(repository_name).each_value { |relationship| relationship.child_key }
+        model.properties(repository_name).each do |property|
+          properties << property unless properties.has_property?(property.name)
         end
-        props
       end
+      properties
     end
 
     def key(repository_name = default_repository_name)
