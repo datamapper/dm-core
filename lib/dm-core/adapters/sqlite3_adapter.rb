b26e3339095b48c0bad6e22974b0849c773cb86a
Daniel Neighman <has.sox@gmail.com>
2008-07-08 00:02:08 +1000

    change ed40a6f50dab4229405203a3a8ec92ae73cd90c0 broke auto_migrations for sqlite3 only.  Seems to be so the key :serial can be used in columns instead of  :serial?.  This is inconsitent with other adapters so the change has been reverted :(

--- lib/dm-core/adapters/sqlite3_adapter.rb
+++ lib/dm-core/adapters/sqlite3_adapter.rb
@@ -71,7 +71,7 @@ module DataMapper
           # TODO: move to dm-more/dm-migrations
           def property_schema_statement(schema)
             statement = super
-            statement << ' PRIMARY KEY AUTOINCREMENT' if supports_serial? && schema[:serial]
+            statement << ' PRIMARY KEY AUTOINCREMENT' if supports_serial? && schema[:serial?]
             statement
           end
 
