c6f52bd3f5990af08837cc8508046e241e211886
Dirkjan Bussink <d.bussink@gmail.com>
2008-09-22 20:35:34 +0200

    Rework Rakefile for CI

--- tasks/ci.rb
+++ tasks/ci.rb
@@ -10,21 +10,9 @@ namespace :ci do
     mkdir_p ROOT + "ci/token"
   end
 
-  Spec::Rake::SpecTask.new("spec:unit" => :prepare) do |t|
-    t.spec_opts = ["--format", "specdoc", "--format", "html:#{ROOT}/ci/unit_rspec_report.html", "--diff"]
-    t.spec_files = Pathname.glob((ROOT + 'spec/unit/**/*_spec.rb').to_s)
-    unless ENV['NO_RCOV']
-      t.rcov = true
-      t.rcov_opts << '--exclude' << "spec,gems"
-      t.rcov_opts << '--text-summary'
-      t.rcov_opts << '--sort' << 'coverage' << '--sort-reverse'
-      t.rcov_opts << '--only-uncovered'
-    end
-  end
-
-  Spec::Rake::SpecTask.new("spec:integration" => :prepare) do |t|
-    t.spec_opts = ["--format", "specdoc", "--format", "html:#{ROOT}/ci/integration_rspec_report.html", "--diff"]
-    t.spec_files = Pathname.glob((ROOT + 'spec/integration/**/*_spec.rb').to_s)
+  Spec::Rake::SpecTask.new(:spec => :prepare) do |t|
+    t.spec_opts = ["--colour", "--format", "specdoc", "--format", "html:#{ROOT}/ci/rspec_report.html", "--diff"]
+    t.spec_files = Pathname.glob((ROOT + 'spec/**/*_spec.rb').to_s)
     unless ENV['NO_RCOV']
       t.rcov = true
       t.rcov_opts << '--exclude' << "spec,gems"
@@ -36,10 +24,7 @@ namespace :ci do
 
   task :spec do
     Rake::Task["ci:spec:unit"].invoke
-    mv ROOT + "coverage", ROOT + "ci/unit_coverage"
-
-    Rake::Task["ci:spec:integration"].invoke
-    mv ROOT + "coverage", ROOT + "ci/integration_coverage"
+    mv ROOT + "coverage", ROOT + "ci/coverage"
   end
 
   task :saikuro => :prepare do
@@ -50,19 +35,7 @@ namespace :ci do
     mv 'ci/token/index_token.html', 'ci/token/index.html'
   end
 
-  task :publish do
-    out = ENV['CC_BUILD_ARTIFACTS'] || "out"
-    mkdir_p out unless File.directory? out
-
-    mv "ci/unit_rspec_report.html", "#{out}/unit_rspec_report.html"
-    mv "ci/unit_coverage", "#{out}/unit_coverage"
-    mv "ci/integration_rspec_report.html", "#{out}/integration_rspec_report.html"
-    mv "ci/integration_coverage", "#{out}/integration_coverage"
-    mv "ci/doc", "#{out}/doc"
-    mv "ci/cyclomatic", "#{out}/cyclomatic_complexity"
-    mv "ci/token", "#{out}/token_complexity"
-  end
 end
 
 #task :ci => %w[ ci:spec ci:doc ci:saikuro install ci:publish ]  # yard-related tasks do not work yet
-task :ci => %w[ ci:spec ci:saikuro install ]
+task :ci => %w[ ci:spec ]
