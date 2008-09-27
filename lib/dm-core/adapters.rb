de479d9658e8af83aa89bc832ac73bbf8e6c4ed6
Dirkjan Bussink <d.bussink@gmail.com>
2008-09-27 14:43:11 +0200

    Don't lazy load the adapters for now, it has nasty consequences

--- lib/dm-core/adapters.rb
+++ lib/dm-core/adapters.rb
@@ -2,3 +2,6 @@ dir = Pathname(__FILE__).dirname.expand_path / 'adapters'
 
 require dir / 'abstract_adapter'
 require dir / 'data_objects_adapter'
+require dir / 'sqlite3_adapter'
+require dir / 'mysql_adapter'
+require dir / 'postgres_adapter'
