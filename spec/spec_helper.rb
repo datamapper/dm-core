361083554502d69808c36dc46d576313bad58a7f
Paul Sadauskas <psadauskas@gmail.com>
2008-10-12 19:23:05 -0600

    Blowing away all the old, crummy, busted-ass specs. Make way for the new hotness

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,18 +6,8 @@ require 'pathname'
 SPEC_ROOT = Pathname(__FILE__).dirname.expand_path
 require SPEC_ROOT.parent + 'lib/dm-core'
 
-# Load the various helpers for the spec suite
-Dir[DataMapper.root / 'spec' / 'lib' / '*.rb'].each do |file|
-  require file
-end
-
-# setup mock adapters
+# setup mock testing adapter
 DataMapper.setup(:default, :adapter => :in_memory)
-DataMapper.setup(:default2, :adapter => :in_memory)
-
-[ :mock, :legacy, :west_coast, :east_coast ].each do |repository_name|
-  DataMapper.setup(repository_name, "mock://localhost/#{repository_name}")
-end
 
 # These environment variables will override the default connection string:
 #   MYSQL_SPEC_URI
@@ -49,38 +39,3 @@ HAS_POSTGRES = setup_adapter(:postgres, 'postgres://postgres@localhost/dm_core_t
 
 DataMapper::Logger.new(nil, :debug)
 
-# ----------------------------------------------------------------------
-# --- Do not declare new models unless absolutely necessary. Instead ---
-# --- pick a metaphor and use those models. If you do need new       ---
-# --- models, define them according to the metaphor being used.      ---
-# ----------------------------------------------------------------------
-
-Spec::Runner.configure do |config|
-  config.before(:each) do
-    # load_models_for_metaphor :vehicles
-  end
-end
-
-# ----------------------------------------------------------------------
-# --- All these models are going to be removed. Don't use them!!!    ---
-# ----------------------------------------------------------------------
-
-class Article
-  include DataMapper::Resource
-
-  property :id,         Serial
-  property :blog_id,    Integer
-  property :created_at, DateTime
-  property :author,     String
-  property :title,      String
-end
-
-class Comment
-  include DataMapper::Resource
-
-  property :id,         Serial # blah
-end
-
-class NormalClass
-  # should not include DataMapper::Resource
-end
