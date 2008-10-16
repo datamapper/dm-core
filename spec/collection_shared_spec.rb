a6ca24a91cc2217a6b37591a35785ec11ed4c04d
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-10-15 21:28:30 -0700

    Attempted different approach to skip Collection#delete_if specs for old extlib

--- spec/collection_shared_spec.rb
+++ spec/collection_shared_spec.rb
@@ -373,7 +373,7 @@ share_examples_for 'A Collection' do
 
   describe '#delete_if' do
     before do
-      pending 'Needs extlib > 0.9.7' unless LazyArray.instance_methods(false).include?('delete_if')
+      pending 'Needs extlib > 0.9.7' unless LazyArray::RETURN_SELF.include?(:delete_if)
     end
 
     describe 'with a block that matches a Resource in the Collection' do
