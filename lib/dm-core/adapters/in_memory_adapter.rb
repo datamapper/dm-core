c5dda391e67d463e8e3d3e3ba643fa3a24906239
Paul Sadauskas <psadauskas@gmail.com>
2008-10-22 12:52:47 -0600

    Documentation for the in-memory adapter.
    To the rejoicing of potential adapter writers everywhere, there is
    now some basic documentation of what the required adapter methods
    do, what they expect, and what they sould be returning.
    
    It is critical that we keep this up-to-date and detailed.

--- lib/dm-core/adapters/in_memory_adapter.rb
+++ lib/dm-core/adapters/in_memory_adapter.rb
@@ -1,7 +1,26 @@
 module DataMapper
   module Adapters
+    # This is probably the simplest functional adapter possible. It simply
+    # stores and queries from a hash containing the model classes as keys, 
+    # and an array of records. It is not persitent whatsoever; when the ruby
+    # process finishes, everything that was stored it lost. However, it doesn't
+    # require any other external libraries, such as data_objects, so it is ideal
+    # for writing specs against. It also serves as an excellent example for
+    # budding adapter developers, so it is critical that it remains well documented
+    # and up to date.
     class InMemoryAdapter < AbstractAdapter
-      # TODO: document
+      ##
+      # Used by DataMapper to put records into a data-store: "INSERT" in SQL-speak.
+      # It takes an array of the resources (model instances) to be saved. Resources
+      # each have a key that can be used to quickly look them up later without 
+      # searching, if the adapter supports it.
+      #
+      # @param [Array] resources
+      #   The set of resources (model instances) 
+      #
+      # @return [Integer] 
+      #   The number of records that were actually saved into the data-store
+      #
       # @api semipublic
       def create(resources)
         repository_name = self.name
@@ -16,7 +35,19 @@ module DataMapper
         end.size # just return the number of records
       end
 
-      # TODO: document
+      ## 
+      # Used by DataMapper to update the attributes on existing records in a
+      # data-store: "UPDATE" in SQL-speak. It takes a hash of the attributes 
+      # to update with, as well as a query object that specifies which resources
+      # should be updated.
+      #
+      # @param [Hash] attributes
+      #   A set of key-value pairs of the attributes to update the resources with.
+      # @param [DataMapper::Query] query
+      #   The query that should be used to find the resource(s) to update.
+      #
+      # @return [Integer] the number of records that were successfully updated
+      #
       # @api semipublic
       def update(attributes, query)
         repository_name = query.repository.name
@@ -28,13 +59,34 @@ module DataMapper
         end.size
       end
 
-      # TODO: document
+      ## 
+      # Look up a single record from the data-store. "SELECT ... LIMIT 1" in SQL.
+      # Used by Model#get to find a record by its identifier(s), and Model#first
+      # to find a single record by some search query.
+      #
+      # @param [DataMapper::Query] query
+      #   The query to be used to locate the resource.
+      #
+      # @return [DataMapper::Resource]
+      #   A Resource object representing the record that was found, or nil for no
+      #   matching records.
+      #
       # @api semipublic
       def read_one(query)
         read(query, query.model, false)
       end
 
-      # TODO: document
+      ## 
+      # Looks up a collection of records from the data-store: "SELECT" in SQL.
+      # Used by Model#all to search for a set of records; that set is in a 
+      # DataMapper::Collection object.
+      #
+      # @param [DataMapper::Query] query
+      #   The query to be used to seach for the resources
+      #
+      # @return [DataMapper::Collection]
+      #   A collection of all the resources found by the query.
+      #
       # @api semipublic
       def read_many(query)
         Collection.new(query) do |set|
@@ -42,7 +94,15 @@ module DataMapper
         end
       end
 
-      # TODO: document
+      ##
+      # Destroys all the records matching the given query. "DELETE" in SQL.
+      #
+      # @param [DataMapper::Query] query
+      #   The query used to locate the resources to be deleted.
+      #
+      # @return [Integer] 
+      #   The number of records that were deleted.
+      #
       # @api semipublic
       def delete(query)
         records = @records[query.model]
@@ -54,14 +114,36 @@ module DataMapper
 
       private
 
-      # TODO: document
+      ##
+      # Make a new instance of the adapter. The @records ivar is the 'data-store'
+      # for this adapter. It is not shared amongst multiple incarnations of this
+      # adapter, eg DataMapper.setup(:default, :adapter => :in_memory); 
+      # DataMapper.setup(:alternate, :adapter => :in_memory) do not share the
+      # data-store between them.
+      #
+      # @param <String, Symbol> name
+      #   The name of the DataMapper::Repository using this adapter.
+      # @param <String, Hash> uri_or_options
+      #   The connection uri string, or a hash of options to set up 
+      #   the adapter
+      #
       # @api semipublic
       def initialize(name, uri_or_options)
         super
         @records = Hash.new { |hash,model| hash[model] = {} }
       end
 
-      # TODO: document
+      ##
+      # #TODO: Extract this into its own module, so it can be re-used in all
+      #        adapters that don't have a native query language
+      #
+      # This is the normal way of parsing the DataMapper::Query object into
+      # a set of conditions. This particular one translates it into ruby code
+      # that can be performed on ruby objects. It can be reused in most other 
+      # adapters, however, if the adapter has its own native query language, 
+      # such as SQL, an adapter developer is probably better using this as an 
+      # example of how to parse the DataMapper::Query object.
+      #
       # @api private
       def read(query, set, many = true)
         repository_name = query.repository.name
