b6a4a7174db73af88ca93111dd8969e4081796bf
Dirkjan Bussink <d.bussink@gmail.com>
2008-11-04 21:05:16 +0100

    Add some useful specs to associations_spec

--- spec/public/associations_spec.rb
+++ spec/public/associations_spec.rb
@@ -23,105 +23,170 @@ describe DataMapper::Associations do
     end
   end
 
-  it "should respond to #has" do
-    Car.should respond_to(:has)
-  end
+  supported_by :all do
 
-  describe "#has" do
-    before do
-      class Car
-        def self.warn
-          # silence warnings
-        end
-      end
+
+    it "should respond to #has" do
+      Car.should respond_to(:has)
     end
 
-    def n
-      Car.n
-    end
+    describe "#has" do
 
-    it "should raise an ArgumentError if the cardinality is not understood" do
-      lambda { Car.has(n..n, :doors) }.should raise_error(ArgumentError)
-    end
-
-    it "should raise an ArgumentError if the minimum constraint is larger than the maximum" do
-      lambda { Car.has(3..1, :doors) }.should raise_error(ArgumentError)
-    end
-
-    describe "1" do
-      before do
-        @relationship = Car.has(1, :engine)
+      def n
+        Car.n
       end
 
-      it "should return a DataMapper::Associations::Relationship" do
-        @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
+      it "should raise an ArgumentError if the cardinality is not understood" do
+        lambda { Car.has(n..n, :doors) }.should raise_error(ArgumentError)
       end
 
-      it "should return a relationship with the child model" do
-        @relationship.child_model.should == Engine
-      end
-    end
-
-    describe "n..n" do
-      before do
-        @relationship = Car.has(1..4, :doors)
+      it "should raise an ArgumentError if the minimum constraint is larger than the maximum" do
+        lambda { Car.has(3..1, :doors) }.should raise_error(ArgumentError)
       end
 
-      it "should create a new relationship" do
-        @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
-      end
-
-      it "should be a relationship with the child model" do
-        @relationship.child_model.should == Door
-      end
-
-      describe "through" do
+      describe "1" do
         before do
-          @relationship = Car.has(1..4, :windows, :through => :doors)
+          @relationship = Car.has(1, :engine)
         end
 
-        it "should return a new relationship" do
-          @relationship.should be_a_kind_of(DataMapper::Associations::RelationshipChain)
-        end
-      end
-    end
+        it 'should add the engine setter'
+        it 'should add the engine getter'
 
-    describe "n" do
-      before do
-        @relationship = Car.has(n, :doors)
       end
 
-      it "should be a relationship with the child model" do
-        @relationship.child_model.should == Door
-      end
-
-      describe "through" do
+      describe "n..n" do
         before do
-          @relationship = Car.has(n, :windows, :through => :doors)
+          @relationship = Car.has(1..4, :doors)
         end
 
-        it "should return a new relationship" do
-          @relationship.should be_a_kind_of(DataMapper::Associations::RelationshipChain)
+        it 'should add the doors getter'
+
+        describe "through" do
+          before do
+            @relationship = Car.has(1..4, :windows, :through => :doors)
+          end
+
+          it 'should add the windows getter'
+        end
+      end
+
+      describe "n" do
+        before do
+          @relationship = Car.has(n, :doors)
+        end
+
+        it 'should add the doors getter'
+
+        describe "through" do
+          before do
+            @relationship = Car.has(n, :windows, :through => :doors)
+          end
+
+          it 'should add the windows getter'
         end
       end
     end
-  end
 
-  it "should respond to #belongs_to" do
-    Engine.should respond_to(:belongs_to)
-  end
-
-  describe "#belongs_to" do
-    before do
-      @relationship = Engine.belongs_to(:car)
+    it "should respond to #belongs_to" do
+      Engine.should respond_to(:belongs_to)
     end
 
-    it "should return a new relationship" do
-      @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
-    end
+    describe "#belongs_to" do
+      before do
+        @relationship = Engine.belongs_to(:car)
+        @other_relationship = Car.has(Car.n, :engines)
+        @engine       = Engine.new
+      end
 
-    it "should return the relationship with the parent model" do
-      @relationship.parent_model.should == Car
+      it 'should add the car setter' do
+        @engine.should respond_to(:car=)
+      end
+
+      it 'should add the car getter' do
+        @engine.should respond_to(:car)
+      end
+
+      it 'should add the car_id foreign key setter' do
+        @engine.should respond_to(:car_id=)
+      end
+
+      it 'should add the car_id foreign key getter' do
+        @engine.should respond_to(:car_id)
+      end
+
+      describe 'changing the parent resource' do
+
+        before do
+          @car = Car.create
+          @engine = Engine.new
+          @engine.car = @car
+        end
+
+        it 'should set the associated foreign key' do
+          @engine.car_id.should == @car.id
+        end
+
+        it 'should add the engine object to the car' do
+          pending "Changing a belongs_to parent should add the object to the correct association"
+          @car.engines.should include(@engine)
+        end
+
+      end
+
+      describe 'changing the parent foreign key' do
+
+        before do
+          @car = Car.create
+
+          @engine = Engine.new
+          @engine.car_id = @car.id
+        end
+
+        it 'should set the associated resource' do
+          @engine.car.should eql(@car)
+        end
+
+      end
+
+      describe 'changing an existing resource through the relation' do
+
+        before do
+          @car1 = Car.create
+          @car2 = Car.create
+          @engine = Engine.create(:car => @car1)
+          @engine.car = @car2
+        end
+
+        it 'should also change the foreign key' do
+          @engine.car_id.should == @car2.id
+        end
+
+        it 'should add the engine to the car' do
+          pending "Changing a belongs_to parent should add the object to the correct association"
+          @car2.engines.should include(@engine)
+        end
+
+      end
+
+      describe 'changing an existing resource through the relation' do
+
+        before do
+          @car1 = Car.create
+          @car2 = Car.create
+          @engine = Engine.create(:car => @car1)
+          @engine.car_id = @car2.id
+        end
+
+        it 'should also change the foreign key' do
+          pending "a change to the foreign key should also change the related object"
+          @engine.car.should eql(@car2)
+        end
+
+        it 'should add the engine to the car' do
+          @car2.engines.include(@engine)
+        end
+
+      end
     end
   end
 end
---
+++ spec/semipublic/associations_spec.rb
@@ -0,0 +1,105 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe DataMapper::Associations do
+  before do
+    class Car
+      include DataMapper::Resource
+      property :id, Serial
+    end
+
+    class Engine
+      include DataMapper::Resource
+      property :id, Serial
+    end
+
+    class Door
+      include DataMapper::Resource
+      property :id, Serial
+    end
+
+    class Window
+      include DataMapper::Resource
+      property :id, Serial
+    end
+  end
+
+  describe "#has" do
+
+    def n
+      Car.n
+    end
+
+    describe "1" do
+      before do
+        @relationship = Car.has(1, :engine)
+      end
+
+      it "should return a DataMapper::Associations::Relationship" do
+        @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
+      end
+
+      it "should return a relationship with the child model" do
+        @relationship.child_model.should == Engine
+      end
+    end
+
+    describe "n..n" do
+      before do
+        @relationship = Car.has(1..4, :doors)
+      end
+
+      it "should create a new relationship" do
+        @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
+      end
+
+      it "should be a relationship with the child model" do
+        @relationship.child_model.should == Door
+      end
+
+      describe "through" do
+        before do
+          @relationship = Car.has(1..4, :windows, :through => :doors)
+        end
+
+        it "should return a new relationship" do
+          @relationship.should be_a_kind_of(DataMapper::Associations::RelationshipChain)
+        end
+      end
+    end
+
+    describe "n" do
+      before do
+        @relationship = Car.has(n, :doors)
+      end
+
+      it "should be a relationship with the child model" do
+        @relationship.child_model.should == Door
+      end
+
+      describe "through" do
+        before do
+          @relationship = Car.has(n, :windows, :through => :doors)
+        end
+
+        it "should return a new relationship" do
+          @relationship.should be_a_kind_of(DataMapper::Associations::RelationshipChain)
+        end
+      end
+    end
+  end
+
+  describe "#belongs_to" do
+    before do
+      @relationship = Engine.belongs_to(:car)
+    end
+
+    it "should return a new relationship" do
+      @relationship.should be_a_kind_of(DataMapper::Associations::Relationship)
+    end
+
+    it "should return the relationship with the parent model" do
+      @relationship.parent_model.should == Car
+    end
+
+  end
+end
