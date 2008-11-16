8abe5228f1d632919da9cffbea6d9af17e32b448
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-11-16 02:17:01 -0800

    Simplification of adapter loading

--- lib/dm-core/adapters.rb
+++ lib/dm-core/adapters.rb
@@ -1,23 +1,9 @@
 dir = Pathname(__FILE__).dirname.expand_path / 'adapters'
 
-require dir / 'abstract_adapter'
-require dir / 'in_memory_adapter'
-
-def try_loading(adapter)
+%w[ abstract in_memory data_objects sqlite3 mysql postgres ].each do |name|
   begin
-    require adapter
+    require dir / "#{name}_adapter"
   rescue LoadError
     # Ignore it
   end
-end
-
-# TODO Factor these out into dm-more
-
-%w[
-  data_objects_adapter
-  sqlite3_adapter
-  mysql_adapter
-  postgres_adapter
-].each do |adapter|
-  try_loading(dir / adapter)
 end
