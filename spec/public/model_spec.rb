7b0d41a0136ea1f5e612d3edda0b77f134df5a34
Paul Sadauskas <pjs@absolute-performance.com>
2008-11-19 09:48:06 -0700

    Move the things that were on in_mem_adapter_spec into model_spec, because thats where they belong

--- spec/public/model_spec.rb
+++ spec/public/model_spec.rb
@@ -2,29 +2,81 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe DataMapper::Model do
   before do
-    # class EmptyModel
-    #   include DataMapper::Resource
+    Object.send(:remove_const, :Heffalump) if defined?(Heffalump)
+    class Heffalump
+      include DataMapper::Resource
 
-    #   property :id, Integer, :key => true
-    # end
+      property :id,         Serial
+      property :color,      String
+      property :num_spots,  Integer
+      property :striped,    Boolean
+    end
+
   end
 
-  describe "#append_extensions" do
+  supported_by :all do
+
     before do
-      module Extender; def foobar; end; end
-      DataMapper::Model.append_extensions(Extender)
+      @heff1 = Heffalump.create(:color => 'Black',     :num_spots => 0,   :striped => true)
+      @heff2 = Heffalump.create(:color => 'Brown',     :num_spots => 25,  :striped => false)
+      @heff3 = Heffalump.create(:color => 'Dark Blue', :num_spots => nil, :striped => false)
     end
 
-    it "should append the module given when DM::Model is extended" do
-      Object.send(:remove_const, :ExtendMe) if defined?(ExtendMe)
-      class ExtendMe
-        include DataMapper::Resource
-        property :id, Integer, :key => true
-      end
-
-      ExtendMe.should respond_to(:foobar)
+    it 'should successfully save an object' do
+      @heff1.new_record?.should be_false
     end
+
+    it 'should be able to get the object' do
+      Heffalump.get(1).should == @heff1
+    end
+
+    it 'should be able to get all the objects' do
+      Heffalump.all.should == [@heff1, @heff2, @heff3]
+    end
+
+    it 'should be able to search for objects with equal value' do
+      Heffalump.all(:striped => true).should == [@heff1]
+    end
+
+    it 'should be able to search for objects included in an array of values' do
+      Heffalump.all(:num_spots => [ 25, 50, 75, 100 ]).should == [@heff2]
+    end
+
+    it 'should be able to search for objects included in a range of values' do
+      Heffalump.all(:num_spots => 25..100).should == [@heff2]
+    end
+
+    it 'should be able to search for objects with nil value' do
+      Heffalump.all(:num_spots => nil).should == [@heff3]
+    end
+
+    it 'should be able to search for objects with not equal value' do
+      Heffalump.all(:striped.not => true).should == [@heff2, @heff3]
+    end
+
+    it 'should be able to search for objects with value less than or equal to' do
+      Heffalump.all(:num_spots.lte => 0).should == [@heff1]
+    end
+
+    it 'should be able to order the objects ascending' do
+      Heffalump.all(:order => [ :color ]).should == [@heff1, @heff2, @heff3]
+    end
+
+    it 'should be able to order the objects descending' do
+      Heffalump.all(:order => [ :color.desc ]).should == [@heff3, @heff2, @heff1]
+    end
+
+    it 'should be able to update an object' do
+      @heff1.num_spots = 10
+      @heff1.save
+      Heffalump.get(1).num_spots.should == 10
+    end
+
+    it 'should be able to destroy an object' do
+      @heff1.destroy
+      Heffalump.all.size.should == 2
+    end
+
   end
-
 
 end
