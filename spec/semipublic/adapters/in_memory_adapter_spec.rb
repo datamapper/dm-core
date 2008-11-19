577dbe5474cdb9d38363832a851d0db7257c9f61
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-11-18 22:22:25 -0800

    Stripped whitespace with "sake strip"

--- spec/semipublic/adapters/in_memory_adapter_spec.rb
+++ spec/semipublic/adapters/in_memory_adapter_spec.rb
@@ -21,57 +21,57 @@ describe DataMapper::Adapters::InMemoryAdapter do
       @string_property = @model.color
       @integer_property = @model.num_spots
     end
-    
+
     it 'should successfully save an object' do
       @heff1.new_record?.should be_false
     end
-    
+
     it 'should be able to get the object' do
       Heffalump.get(1).should == @heff1
     end
-    
+
     it 'should be able to get all the objects' do
       Heffalump.all.should == [@heff1, @heff2, @heff3]
     end
-    
+
     it 'should be able to search for objects with equal value' do
       Heffalump.all(:striped => true).should == [@heff1]
     end
-    
+
     it 'should be able to search for objects included in an array of values' do
       Heffalump.all(:num_spots => [ 25, 50, 75, 100 ]).should == [@heff2]
     end
-    
+
     it 'should be able to search for objects included in a range of values' do
       Heffalump.all(:num_spots => 25..100).should == [@heff2]
     end
-    
+
     it 'should be able to search for objects with nil value' do
       Heffalump.all(:num_spots => nil).should == [@heff3]
     end
-    
+
     it 'should be able to search for objects with not equal value' do
       Heffalump.all(:striped.not => true).should == [@heff2, @heff3]
     end
-    
+
     it 'should be able to search for objects with value less than or equal to' do
       Heffalump.all(:num_spots.lte => 0).should == [@heff1]
     end
-    
+
     it 'should be able to order the objects ascending' do
       Heffalump.all(:order => [ :color ]).should == [@heff1, @heff2, @heff3]
     end
-    
+
     it 'should be able to order the objects descending' do
       Heffalump.all(:order => [ :color.desc ]).should == [@heff3, @heff2, @heff1]
     end
-    
+
     it 'should be able to update an object' do
       @heff1.num_spots = 10
       @heff1.save
       Heffalump.get(1).num_spots.should == 10
     end
-    
+
     it 'should be able to destroy an object' do
       @heff1.destroy
       Heffalump.all.size.should == 2
