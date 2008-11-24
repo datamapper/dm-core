da60b4ded1c735d74048d9685b37c8876a84f8fb
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2008-11-23 21:02:42 -0800

    Updated Repository API docs to YARD format.

--- lib/dm-core/repository.rb
+++ lib/dm-core/repository.rb
@@ -5,22 +5,38 @@ module DataMapper
     @adapters = {}
 
     ##
+    # Get the list of adapters registered for all Repositories,
+    # keyed by repository name.
     #
-    # @return <Adapter> the adapters registered for this repository
+    # @return [Hash<Symbol => DataMapper::Adapters::AbstractAdapter>]
+    #   the adapters registered for all Repositories
     def self.adapters
       @adapters
     end
 
+    ##
+    # Get the stack of current repository contexts
     def self.context
       Thread.current[:dm_repository_contexts] ||= []
     end
 
+    # Get the default name of this Repository
+    # @return [Symbol] the default name of this repository
+    # @api ???
     def self.default_name
       :default
     end
 
     attr_reader :name
 
+    # Get the adapter for this repository
+    # 
+    # Lazy loads adapter setup from registered adapters
+    # @return [DataMapper::Adapters::AbstractAdapter]
+    #   the adapter for this repository
+    # @raise [ArgumentError]
+    #   if there is no adapter registered for a repository with name +@name+
+    # @api ???
     def adapter
       # Make adapter instantiation lazy so we can defer repository setup until it's actually
       # needed. Do not remove this code.
@@ -32,6 +48,13 @@ module DataMapper
       end
     end
 
+    ##
+    # Get the identity for a particular model within this repository.
+    # 
+    # If one doesn't yet exist, create a new default in-memory IdentityMap
+    # for the requested model.
+    # 
+    # TODO: allow setting default secondary IdentityMap (eg., Memcache, I hope)
     def identity_map(model)
       @identity_maps[model] ||= IdentityMap.new
     end
@@ -47,6 +70,14 @@ module DataMapper
       end
     end
 
+    ##
+    # Create one or more resource instances in this repository.
+    #
+    # @param [Array<DataMapper::Resource>] resources
+    #   The list of resources (model instances) to create
+    # @return [Integer]
+    #   The number of records that were actually saved into the data-store
+    # @api semipublic
     def create(resources)
       adapter.create(resources)
     end
@@ -54,32 +85,54 @@ module DataMapper
     ##
     # retrieve a collection of results of a query
     #
-    # @param <Query> query composition of the query to perform
-    # @return <DataMapper::Collection> result set of the query
+    # @param [Query] query composition of the query to perform
+    # @return [DataMapper::Collection] result set of the query
+    # @return [NilClass] no object could be found which matches that query
     # @see DataMapper::Query
     def read_many(query)
       adapter.read_many(query)
     end
 
     ##
-    # retrieve a resource instance by a query
+    # retrieve a single resource instance by a query
     #
-    # @param <Query> query composition of the query to perform
-    # @return <DataMapper::Resource> the first retrieved instance which matches the query
-    # @return <NilClass> no object could be found which matches that query
+    # @param [DataMapper::Query] query composition of the query to perform
+    # @return [DataMapper::Resource] the first retrieved instance which matches the query
+    # @return [NilClass] no object could be found which matches that query
     # @see DataMapper::Query
     def read_one(query)
       adapter.read_one(query)
     end
 
+    ##
+    # Update the attributes of one or more resource instances
+    # 
+    # @param [Hash<DataMapper::Property => Object>] attributes
+    #   hash of attribute values to set, keyed by Property
+    # @param [DataMapper::Query] query specifies which records are to be updated
+    # @return [Integer] the number of records updated
+    # @see DataMapper::Query
     def update(attributes, query)
       adapter.update(attributes, query)
     end
 
+    ##
+    # Delete one or more resource instances
+    # 
+    # @param [DataMapper::Query] query specifies which records are to be deleted
+    # @return [Integer] the number of records deleted
+    # @see DataMapper::Query
     def delete(query)
       adapter.delete(query)
     end
 
+    ##
+    # Test whether this repository equals +other+. Repositories are equal if
+    # they have the same name.
+    # 
+    # @param [Object] other object to be compared to self
+    # @return [TrueClass, FalseClass] whether self equals +other+
+    # @api semipublic
     def eql?(other)
       return true if super
       name == other.name
@@ -87,6 +140,9 @@ module DataMapper
 
     alias == eql?
 
+    # Get a concise, human readalbe representation of this repository
+    # @return [String] concise human readable representation of this repository
+    # @api private
     def to_s
       "#<DataMapper::Repository:#{@name}>"
     end
@@ -113,8 +169,10 @@ module DataMapper
       end
 
       ##
+      # Determine whether a particular named storage exists in this repository
       #
-      # @return <True, False> whether or not the data-store exists for this repo
+      # @param [String] storage_name name of the storage to test for
+      # @return [TrueClass, FalseClass] true if the data-store +storage_name+ exists
       #
       # TODO: move to dm-more/dm-migrations
       def storage_exists?(storage_name)
