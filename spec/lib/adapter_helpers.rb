17b5104fbc8b856f35bba8813bddb5aaaf9a49f6
Carl Lerche <carllerche@mac.com>
2008-11-26 20:47:58 -0800

    Transaction specs beginning and spec helper bug fix

--- spec/lib/adapter_helpers.rb
+++ spec/lib/adapter_helpers.rb
@@ -4,16 +4,14 @@ module DataMapper::Spec
       @current_adapters ||= []
     end
 
-    def supported_by(*adapters)
+    def supported_by(*adapters, &block)
       adapters = get_adapters(*adapters)
 
       PRIMARY.only(*adapters).each do |adapter, connection_uri|
         # keep track of the current adapters
         AdapterHelpers.current_adapters << adapters
 
-        message = "with #{adapter}" if adapters.length > 1
-
-        with_adapter_spec_wrapper(message) do
+        describe("with #{adapter}") do
 
           before do
             # store these in instance vars for the shared adapter specs
@@ -37,19 +35,18 @@ module DataMapper::Spec
             end
           end
 
-          yield adapter
+          self.instance_eval(&block)
         end
 
         AdapterHelpers.current_adapters.pop
       end
     end
 
-    def with_alternate_adapter
+    def with_alternate_adapter(&block)
       adapters = AdapterHelpers.current_adapters.last
 
       ALTERNATE.only(*adapters).each do |adapter, connection_uri|
-        message = "and #{adapter}" if adapters.length > 1
-        with_adapter_spec_wrapper(message) do
+        describe("and #{adapter}") do
 
           before do
             @alternate_adapter = DataMapper.setup(:alternate, connection_uri)
@@ -71,7 +68,7 @@ module DataMapper::Spec
             end
           end
 
-          yield adapter
+          self.instance_eval(&block)
         end
       end
     end
@@ -80,16 +77,6 @@ module DataMapper::Spec
       adapters.map! { |a| a.to_s }
       adapters = ADAPTERS if adapters.include?('all')
       ADAPTERS & adapters
-    end
-
-    def with_adapter_spec_wrapper(message)
-      if message
-        describe(message) do
-          yield
-        end
-      else
-        yield
-      end
     end
   end
 end
---
+++ spec/public/transaction_spec.rb
@@ -0,0 +1,92 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+
+describe DataMapper::Resource, "Transactions" do
+  
+  before do
+    Object.send(:remove_const, :User) if defined?(User)
+    class User
+      include DataMapper::Resource
+
+      property :name,        String, :key => true
+      property :age,         Integer
+      property :description, Text
+
+      has n, :comments
+    end
+
+    Object.send(:remove_const, :Comment) if defined?(Comment)
+    class Comment
+      include DataMapper::Resource
+
+      property :id,   Serial
+      property :body, Text
+
+      belongs_to :user
+    end
+  end
+  
+  supported_by :postgres, :mysql do
+    before(:each) do
+      # --- Temporary private api use to get around rspec limitations ---
+      repository(:default) do
+        transaction = DataMapper::Transaction.new(repository)
+        transaction.begin
+        repository.adapter.push_transaction(transaction)
+      end
+              
+      @model       = User
+      @child_model = Comment
+      @user        = @model.create(:name => 'dbussink', :age => 25, :description => "Test")
+    end
+    
+    after do
+      repository = repository(:default)
+      while repository.adapter.current_transaction
+        repository.adapter.current_transaction.rollback
+        repository.adapter.pop_transaction
+      end
+    end
+      
+    it_should_behave_like 'A public Resource'
+    
+  end
+  
+  supported_by :postgres, :mysql do
+  
+    describe "#transaction" do
+          
+      it "should have access to resources presisted before the transaction" do
+        User.create(:name => "carllerche")
+        User.transaction do
+          User.first.name.should == "carllerche"
+        end
+      end
+      
+      it "should have access to resources persisted in the transaction" do
+        User.transaction do
+          User.create(:name => "carllerche")
+          User.first.name.should == "carllerche"
+        end
+      end
+      
+      it "should not mark persisted resources as new records" do
+        User.transaction do
+          User.create(:name => "carllerche").should_not be_new_record
+        end
+      end
+      
+      it "should rollback when an error is thrown in a transaction" do
+        lambda {
+          User.transaction do
+            User.create(:name => "carllerche")
+            raise "I love coffee"
+          end
+        }.should raise_error("I love coffee")
+        User.first.should be_nil
+      end
+      
+    end
+    
+  end
+  
+end
