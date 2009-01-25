d21ebcba55cbb8b24dae99168cf46631ae6ef8ca
Dan Kubb <dan.kubb@autopilotmarketing.com>
2009-01-24 19:59:43 -0800

    Changed rake tasks to not be prefixed with dm unecessarily

--- tasks/dm.rb
+++ tasks/dm.rb
@@ -1,72 +1,57 @@
 require 'spec/rake/spectask'
 require 'spec/rake/verify_rcov'
 
-task :default => 'dm:spec'
-task :spec    => 'dm:spec'
-task :rcov    => 'dm:rcov'
+task :default => 'spec'
+
+RCov::VerifyTask.new(:verify_rcov => :rcov) do |t|
+  t.threshold = 82.9 # Make sure you have rcov 0.7 or higher!
+end
+
+def run_spec(name, files, rcov)
+  Spec::Rake::SpecTask.new(name) do |t|
+    ENV['ADAPTERS'] ||= 'all'
+    t.spec_opts << '--options' << ROOT + 'spec/spec.opts'
+    t.spec_files = Pathname.glob(ENV['FILES'] || files.to_s).map { |f| f.to_s }
+    t.rcov = rcov
+    t.rcov_opts << '--exclude' << 'spec'
+    t.rcov_opts << '--text-summary'
+    t.rcov_opts << '--sort' << 'coverage' << '--sort-reverse'
+    t.rcov_opts << '--only-uncovered'
+  end
+end
+
+public_specs     = ROOT + 'spec/public/**/*_spec.rb'
+semipublic_specs = ROOT + 'spec/semipublic/**/*_spec.rb'
+all_specs        = ROOT + 'spec/**/*_spec.rb'
+
+desc "Run all specifications"
+run_spec('spec', all_specs, false)
+
+desc "Run all specifications with rcov"
+run_spec('rcov', all_specs, true)
 
 namespace :spec do
-  task :public     => 'dm:spec:public'
-  task :semipublic => 'dm:spec:semipublic'
+  desc "Run public specifications"
+  run_spec('public', public_specs, false)
+
+  desc "Run semipublic specifications"
+  run_spec('semipublic', semipublic_specs, false)
 end
 
 namespace :rcov do
-  task :public     => 'dm:rcov:public'
-  task :semipublic => 'dm:rcov:semipublic'
+  desc "Run public specifications with rcov"
+  run_spec('public', public_specs, true)
+
+  desc "Run semipublic specifications with rcov"
+  run_spec('semipublic', semipublic_specs, true)
 end
 
-RCov::VerifyTask.new(:verify_rcov => :rcov) do |t|
-  t.threshold = 87.8 # Make sure you have rcov 0.7 or higher!
+desc "Run all comparisons with ActiveRecord"
+task :perf do
+  sh ROOT + 'script/performance.rb'
 end
 
-# TODO: remove the "dm" namespace
-namespace :dm do
-  def run_spec(name, files, rcov)
-    Spec::Rake::SpecTask.new(name) do |t|
-      ENV['ADAPTERS'] ||= 'all'
-      t.spec_opts << '--options' << ROOT + 'spec/spec.opts'
-      t.spec_files = Pathname.glob(ENV['FILES'] || files.to_s).map { |f| f.to_s }
-      t.rcov = rcov
-      t.rcov_opts << '--exclude' << 'spec,environment.rb'
-      t.rcov_opts << '--text-summary'
-      t.rcov_opts << '--sort' << 'coverage' << '--sort-reverse'
-      t.rcov_opts << '--only-uncovered'
-    end
-  end
-
-  public_specs     = ROOT + 'spec/public/**/*_spec.rb'
-  semipublic_specs = ROOT + 'spec/semipublic/**/*_spec.rb'
-  all_specs        = ROOT + 'spec/**/*_spec.rb'
-
-  desc "Run all specifications"
-  run_spec('spec', all_specs, false)
-
-  desc "Run all specifications with rcov"
-  run_spec('rcov', all_specs, true)
-
-  namespace :spec do
-    desc "Run public specifications"
-    run_spec('public', public_specs, false)
-
-    desc "Run semipublic specifications"
-    run_spec('semipublic', semipublic_specs, false)
-  end
-
-  namespace :rcov do
-    desc "Run public specifications with rcov"
-    run_spec('public', public_specs, true)
-
-    desc "Run semipublic specifications with rcov"
-    run_spec('semipublic', semipublic_specs, true)
-  end
-
-  desc "Run all comparisons with ActiveRecord"
-  task :perf do
-    sh ROOT + 'script/performance.rb'
-  end
-
-  desc "Profile DataMapper"
-  task :profile do
-    sh ROOT + 'script/profile.rb'
-  end
+desc "Profile DataMapper"
+task :profile do
+  sh ROOT + 'script/profile.rb'
 end
