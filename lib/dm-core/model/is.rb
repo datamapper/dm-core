9ffb6de9d5465bc5dba96d7aa4af51068f02c367
Michael S. Klishin <michael@novemberain.com>
2009-03-03 08:07:43 +0300

    Document DataMapper::Is

--- lib/dm-core/model/is.rb
+++ lib/dm-core/model/is.rb
@@ -1,6 +1,18 @@
 module DataMapper
+  # Module that provides a common way for plugin authors
+  # to implement "is ... " traits (object behaviors that can be shared)
   module Is
-
+    # A common interface to activate plugins for a resource. For instance:
+    #
+    # class Widget
+    #   include DataMapper::Resource
+    #
+    #   is :list
+    # end
+    #
+    # adds list item behavior to the model. Plugin that wants to conform
+    # to "is API" of DataMapper must supply is_+behavior name+ method,
+    # for example above it would be is_list.
     def is(plugin, *pars, &block)
       generator_method = "is_#{plugin}".to_sym
 
