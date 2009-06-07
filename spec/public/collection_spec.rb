848aa639a1cd1103cb04d509ec840d35db6952a4
Dan Kubb <dan.kubb@gmail.com>
2009-06-06 18:47:07 -0700

    Fixed spec module name to not be namespaced in anonymous module created by rspec

--- spec/public/collection_spec.rb
+++ spec/public/collection_spec.rb
@@ -8,7 +8,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
     self.loaded = loaded
 
     before :all do
-      module Blog
+      module ::Blog
         class Article
           include DataMapper::Resource
 
--- spec/public/model_spec.rb
+++ spec/public/model_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe DataMapper::Model do
   before :all do
-    module Blog
+    module ::Blog
       class Article
         include DataMapper::Resource
 
