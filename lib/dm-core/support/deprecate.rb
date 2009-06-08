d0b83e4e8d5b522e1316c19fb04b4ee3a927dbf5
Dan Kubb <dan.kubb@gmail.com>
2009-06-08 08:05:57 -0700

    Added caller information to deprecation warning

--- lib/dm-core/support/deprecate.rb
+++ lib/dm-core/support/deprecate.rb
@@ -3,7 +3,7 @@ module DataMapper
     def deprecate(old_method, new_method)
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{old_method}(*args, &block)
-          warn "\#{self.class}##{old_method} is deprecated, use \#{self.class}##{new_method} instead"
+          warn "\#{self.class}##{old_method} is deprecated, use \#{self.class}##{new_method} instead (\#{caller[0]})"
           send(#{new_method.inspect}, *args, &block)
         end
       RUBY
