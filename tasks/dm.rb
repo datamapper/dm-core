08d4f6c62254b946782b71c928e26ed728b9279c
Dan Kubb <dan.kubb@gmail.com>
2009-07-04 21:49:17 -0700

    Updated coverage threshold to 88.1%

--- tasks/dm.rb
+++ tasks/dm.rb
@@ -4,7 +4,7 @@ require 'spec/rake/verify_rcov'
 task :default => 'spec'
 
 RCov::VerifyTask.new(:verify_rcov => :rcov) do |config|
-  config.threshold = 88.0 # Make sure you have rcov 0.7 or higher!
+  config.threshold = 88.1  # Make sure you have rcov 0.7 or higher!
 end
 
 def run_spec(name, files, rcov)
