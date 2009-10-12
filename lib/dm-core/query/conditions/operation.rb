0314967d81f1275be5995652aa38ca2e64f7b819
Ted Han <knowtheory@celesta.local>
2009-10-12 11:50:29 -0400

    Accommodating Arrays as operands

--- lib/dm-core/query/conditions/operation.rb
+++ lib/dm-core/query/conditions/operation.rb
@@ -96,7 +96,7 @@ module DataMapper
         # TODO: document
         # @api semipublic
         def <<(operand)
-          unless operand.kind_of? Conditions::AbstractOperation or operand.kind_of? Conditions::AbstractComparison
+          unless operand.kind_of? Conditions::AbstractOperation or operand.kind_of? Conditions::AbstractComparison or operand.kind_of? Array
             raise ArgumentError, "Operands must be a kind of DataMapper::Query::Conditions::AbstractOperation or DataMapper::Query::Conditions::AbstractComparison. This one was a #{operand.class}"
           end
           @operands << operand unless operand.nil?
