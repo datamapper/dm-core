121d3377251de38d21a0262d8bf688ee3b4bda82
Dan Kubb <dan.kubb@gmail.com>
2010-02-03 10:21:11 +0100

    Added spec to ensure Model#property raises an exception with a reserved name
    [#844 state:resolved]

---
+++ spec/public/model/property_spec.rb
@@ -0,0 +1,41 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'spec_helper'))
+
+describe DataMapper::Model::Property do
+  before :all do
+    Object.const_remove(:ModelPropertySpecs) if defined?(ModelPropertySpecs)
+    class ::ModelPropertySpecs
+      include DataMapper::Resource
+
+      property :id, Serial
+    end
+  end
+
+  describe '#property' do
+    after do
+      [ :name, :name= ].each do |method|
+        next unless ModelPropertySpecs.method_defined?(method)
+        ModelPropertySpecs.send(:undef_method, method)
+      end
+    end
+
+    subject { ModelPropertySpecs.property(:name, String) }
+
+    it 'should define a name accessor' do
+      ModelPropertySpecs.should_not be_method_defined(:name)
+      subject
+      ModelPropertySpecs.should be_method_defined(:name)
+    end
+
+    it 'should define a name= mutator' do
+      ModelPropertySpecs.should_not be_method_defined(:name=)
+      subject
+      ModelPropertySpecs.should be_method_defined(:name=)
+    end
+
+    it 'should raise an exception if the method exists' do
+      lambda {
+        ModelPropertySpecs.property(:key, String)
+      }.should raise_error(ArgumentError, '+name+ was :key, which cannot be used as a property name since it collides with an existing method')
+    end
+  end
+end
