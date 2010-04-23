3b2cbcfcdc2723b7b2e000eacb39469184ad6c1d
snusnu <gamsnjaga@gmail.com>
2010-04-23 02:32:43 +0200

    Do not assume presence of the DataMapper constant

--- lib/dm-core/spec/lib/collection_helpers.rb
+++ lib/dm-core/spec/lib/collection_helpers.rb
@@ -1,15 +1,17 @@
-module DataMapper::Spec
-  module CollectionHelpers
-    module GroupMethods
-      def self.extended(base)
-        base.class_inheritable_accessor :loaded
-        base.loaded = false
-      end
+module DataMapper
+  module Spec
+    module CollectionHelpers
+      module GroupMethods
+        def self.extended(base)
+          base.class_inheritable_accessor :loaded
+          base.loaded = false
+        end
 
-      def should_not_be_a_kicker(ivar = :@articles)
-        unless loaded
-          it 'should not be a kicker' do
-            instance_variable_get(ivar).should_not be_loaded
+        def should_not_be_a_kicker(ivar = :@articles)
+          unless loaded
+            it 'should not be a kicker' do
+              instance_variable_get(ivar).should_not be_loaded
+            end
           end
         end
       end
