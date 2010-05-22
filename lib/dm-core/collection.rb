afe1a4988245d652e633402aee675f776ebb5cd0
Dan Kubb <dan.kubb@gmail.com>
2010-05-21 21:55:39 -0700

    Not sure why Model#__send__ is used here. #send will work fine.

--- lib/dm-core/collection.rb
+++ lib/dm-core/collection.rb
@@ -1463,7 +1463,7 @@ module DataMapper
     # @api private
     def delegate_to_model(method, *args, &block)
       model = self.model
-      model.__send__(:with_scope, query) do
+      model.send(:with_scope, query) do
         model.send(method, *args, &block)
       end
     end
