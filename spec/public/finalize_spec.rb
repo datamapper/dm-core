6af161cb4e66525c82ff6945ee0739431bd0827a
Dan Kubb <dan.kubb@gmail.com>
2010-06-05 21:12:43 -0700

    Changed specs to use subject block instead of duplicate lambdas

--- spec/public/finalize_spec.rb
+++ spec/public/finalize_spec.rb
@@ -2,12 +2,14 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe DataMapper do
   describe '.finalize' do
+    subject { DataMapper.finalize }
+
     it "should not raise with valid models" do
       class ::ValidObject
         include DataMapper::Resource
         property :id, Integer, :key => true
       end
-      lambda { DataMapper.finalize }.should_not raise_error
+      method(:subject).should_not raise_error
       DataMapper::Model.descendants.delete(ValidObject)
       Object.send(:remove_const, :ValidObject)
     end
@@ -16,7 +18,7 @@ describe DataMapper do
       class ::EmptyObject
         include DataMapper::Resource
       end
-      lambda { DataMapper.finalize }.should raise_error
+      method(:subject).should raise_error
       DataMapper::Model.descendants.delete(EmptyObject)
       Object.send(:remove_const, :EmptyObject)
     end
@@ -26,7 +28,7 @@ describe DataMapper do
         include DataMapper::Resource
         property :name, String
       end
-      lambda { DataMapper.finalize }.should raise_error
+      method(:subject).should raise_error
       DataMapper::Model.descendants.delete(KeylessObject)
       Object.send(:remove_const, :KeylessObject)
     end
