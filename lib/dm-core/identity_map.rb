a2b1692d4441b413d6e0132f40b3993d3e9144ab
Dan Kubb <dan.kubb@gmail.com>
2011-01-13 22:02:10 -0800

    Remove deprecated methods from IdentityMap

--- lib/dm-core/identity_map.rb
+++ lib/dm-core/identity_map.rb
@@ -3,10 +3,5 @@ module DataMapper
   # Tracks objects to help ensure that each object gets loaded only once.
   # See: http://www.martinfowler.com/eaaCatalog/identityMap.html
   class IdentityMap < Hash
-    extend Deprecate
-
-    deprecate :get, :[]
-    deprecate :set, :[]=
-
   end # class IdentityMap
 end # module DataMapper
