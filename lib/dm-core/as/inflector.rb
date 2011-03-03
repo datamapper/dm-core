79d393dd6d68d2f3ae8b90dd0a377d193aa49f3f
Nico Rieck <nico.rieck@gmail.com>
2011-03-03 10:21:53 +0100

    Move Inflector to support/.

--- lib/dm-core/as/inflector.rb
+++
@@ -1,7 +0,0 @@
-# in case active_support/inflector is required without the rest of active_support
-require 'dm-core/as/inflector/inflections'
-require 'dm-core/as/inflector/transliterate'
-require 'dm-core/as/inflector/methods'
-
-require 'dm-core/as/inflections'
-require 'dm-core/as/core_ext/string/inflections'
---
+++ lib/dm-core/support/inflector.rb
@@ -0,0 +1,4 @@
+require 'dm-core/support/inflector/inflections'
+require 'dm-core/support/inflector/transliterate'
+require 'dm-core/support/inflector/methods'
+require 'dm-core/support/inflections'
