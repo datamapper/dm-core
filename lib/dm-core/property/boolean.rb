9c2b9c1227d7e4ed64056d6baf38bd15af92a578
Piotr Solnica <piotr.solnica@gmail.com>
2011-04-07 19:32:21 +0200

    Use a boolean map to make things a bit faster

--- lib/dm-core/property/boolean.rb
+++ lib/dm-core/property/boolean.rb
@@ -5,8 +5,10 @@ module DataMapper
 
       primitive ::TrueClass
 
-      TRUE_VALUES  = [ 1, '1', 't', 'T', 'true',  'TRUE'  ].to_set.freeze
-      FALSE_VALUES = [ 0, '0', 'f', 'F', 'false', 'FALSE' ].to_set.freeze
+      TRUE_VALUES  = [ 1, '1', 't', 'T', 'true',  'TRUE'  ].freeze
+      FALSE_VALUES = [ 0, '0', 'f', 'F', 'false', 'FALSE' ].freeze
+      BOOLEAN_MAP  = Hash[
+        TRUE_VALUES.product([ true ]) + FALSE_VALUES.product([ false ]) ].freeze
 
       def primitive?(value)
         value == true || value == false
@@ -22,13 +24,7 @@ module DataMapper
       #
       # @api private
       def typecast_to_primitive(value)
-        if TRUE_VALUES.include?(value)
-          true
-        elsif FALSE_VALUES.include?(value)
-          false
-        else
-          value
-        end
+        BOOLEAN_MAP.fetch(value, value)
       end
     end # class Boolean
   end # class Property
