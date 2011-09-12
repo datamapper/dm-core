22b351f4bd15fca08af9e0ee689f1df75bea253c
d11wtq <chris@w3style.co.uk>
2011-09-12 22:59:21 +1000

    Conditionally force encoding based on RUBY_VERSION and dup input values before encoding

--- lib/dm-core/property/binary.rb
+++ lib/dm-core/property/binary.rb
@@ -3,14 +3,18 @@ module DataMapper
     class Binary < String
       include PassThroughLoadDump
 
-      def load(value)
-        super.force_encoding("BINARY") if value
+      if RUBY_VERSION >= "1.9"
+
+        def load(value)
+          super.dup.force_encoding("BINARY") if value
+        end
+
+        def dump(value)
+          super.dup.force_encoding("BINARY") if value
+        end
+
       end
 
-      def dump(value)
-        super.force_encoding("BINARY") if value
-      end
-      
     end # class Binary
   end # class Property
 end # module DataMapper
--- spec/public/property/binary_spec.rb
+++ spec/public/property/binary_spec.rb
@@ -20,20 +20,22 @@ describe DataMapper::Property::Binary do
     it { should eql(:primitive => @primitive, :length => 50) }
   end
 
-  describe 'encoding' do
-    let(:model) do
-      Class.new do
-        include ::DataMapper::Resource
-        property :bin_data, ::DataMapper::Property::Binary
+  if RUBY_VERSION >= "1.9"
+    describe 'encoding' do
+      let(:model) do
+        Class.new do
+          include ::DataMapper::Resource
+          property :bin_data, ::DataMapper::Property::Binary
+        end
       end
-    end
 
-    it 'should always dump with BINARY' do
-      model.bin_data.dump("foo").encoding.names.should include("BINARY")
-    end
+      it 'should always dump with BINARY' do
+        model.bin_data.dump("foo".freeze).encoding.names.should include("BINARY")
+      end
 
-    it 'should always load with BINARY' do
-      model.bin_data.load("foo").encoding.names.should include("BINARY")
+      it 'should always load with BINARY' do
+        model.bin_data.load("foo".freeze).encoding.names.should include("BINARY")
+      end
     end
   end
 end
