d8216cfb3acf4dc72bc5cf4093fa955de0f5a65e
Matt Panaro <mpanaro@primerevenue.com>
2012-07-12 20:33:11 -0400

    add fix for singularization

--- spec/unit/inflections_spec.rb
+++ spec/unit/inflections_spec.rb
@@ -4,13 +4,13 @@ require 'dm-core/support/inflector/inflections'
 describe DataMapper::Inflector do
 
   it "should singularize 'status' correctly" do
-    DataMapper::Inflector.singularize('status').should == 'status'
-    DataMapper::Inflector.singularize('status').should_not == 'statu'
+    DataMapper::Inflector.singularize('status').should eql 'status'
+    DataMapper::Inflector.singularize('status').should_not eql 'statu'
   end
 
   it "should singularize 'alias' correctly" do
-    DataMapper::Inflector.singularize('alias').should == 'alias'
-    DataMapper::Inflector.singularize('alias').should_not eq 'alia'
+    DataMapper::Inflector.singularize('alias').should eql 'alias'
+    DataMapper::Inflector.singularize('alias').should_not eql 'alia'
   end
 
 end
