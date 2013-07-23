b984a56b3b7135082a47088ab487916e90046053
Marco Lisci <info@badshark.io>
2013-07-23 14:29:14 +0200

    Reverted back localhost for connection example

--- lib/dm-core.rb
+++ lib/dm-core.rb
@@ -127,7 +127,7 @@ end
 # == Setup and Configuration
 # DataMapper uses URIs or a connection hash to connect to your data-store.
 # URI connections takes the form of:
-#   DataMapper.setup(:default, 'protocol://username:password@127.0.0.1:port/path/to/repo')
+#   DataMapper.setup(:default, 'protocol://username:password@localhost:port/path/to/repo')
 #
 # Breaking this down, the first argument is the name you wish to give this
 # connection.  If you do not specify one, it will be assigned :default. If you
