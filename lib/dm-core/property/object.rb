28b3185eb54cea150b6716fe8afc674e12908271
Piotr Solnica <piotr.solnica@gmail.com>
2015-03-27 20:30:02 +0100

    Fix Property::Object.dump_as setting

--- lib/dm-core/property/object.rb
+++ lib/dm-core/property/object.rb
@@ -2,7 +2,7 @@ module DataMapper
   class Property
     class Object < Property
       load_as ::Object
-      dump_as ::String
+      dump_as ::Object
 
       # @api semipublic
       def dump(value)
